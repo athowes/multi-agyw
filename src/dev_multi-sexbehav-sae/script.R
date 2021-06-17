d1239e055c3e7eee9712cb404e570949987a4121
Adam Howes <adamhowes@outlook.com>
2021-06-17 23:36:30 +0100

    The age x category interaction works to recover the simulated values

--- src/dev_multi-sexbehav-sae/script.R
+++ src/dev_multi-sexbehav-sae/script.R
@@ -2,6 +2,7 @@
 orderly::orderly_develop_start("dev_multi-sexbehav-sae")
 setwd("src/dev_multi-sexbehav-sae/")
 
+#' Add age specific identifier
 df <- read_csv("depends/simulated-sexbehav.csv")
 
 #' See Serafini's "Multinomial logit models with INLA" tutorial
@@ -9,7 +10,7 @@ df <- read_csv("depends/simulated-sexbehav.csv")
 
 #' Model 1.
 #' No intercept (-1) and observation specific intercepts f(obs_idx, ...)
-formula1 <- y ~ -1 + f(obs_idx, initial = -10, fixed = TRUE)
+formula1 <- y ~ -1 + f(obs_idx, hyper = list(prec = list(initial = log(0.000001), fixed = TRUE)))
 
 fit1 <- inla(formula1, data = df, family = "Poisson",
              control.predictor = list(link = 1),
@@ -30,8 +31,8 @@ fit0$summary.fixed
 
 #' Model 2.
 #' Add category specific random effects
-formula2 <- y ~ -1 + f(obs_idx, initial = -10, fixed = TRUE) +
-  f(cat_idx, model = "iid")
+formula2 <- y ~ -1 + f(obs_idx, hyper = list(prec = list(initial = log(0.000001), fixed = TRUE))) +
+  f(cat_idx, model = "iid", constr = TRUE, hyper = list(prec = list(initial = log(0.001), fixed = TRUE)))
 
 fit2 <- inla(formula2, data = df, family = "Poisson",
              control.predictor = list(link = 1),
@@ -59,30 +60,30 @@ cbind(truth = colMeans(prob), pred = exp(etas) / sum(exp(etas)))
 
 #' Model 3.
 #' Add age specific random effects
-#' Though what is required are age category interactions
 
-#' Start by adding age specific identifier
 df <- mutate(df, age_idx = case_when(
   age_group == "Y015_019" ~ 1,
   age_group == "Y020_024" ~ 2,
-  age_group == "Y025_029" ~ NA_real_
-))
+  age_group == "Y025_029" ~ 3)
+)
 
-formula3 <- y ~ -1 + f(obs_idx, initial = -10, fixed = TRUE) +
-  f(cat_idx, model = "iid", fixed = TRUE, constr = TRUE) +
-  f(age_idx, model = "iid", fixed = TRUE, constr = TRUE)
+formula3 <- y ~ -1 + f(obs_idx, hyper = list(prec = list(initial = log(0.000001), fixed = TRUE))) +
+  f(cat_idx, model = "iid", constr = TRUE, hyper = list(prec = list(initial = log(0.001), fixed = TRUE))) +
+  f(age_idx, model = "iid", constr = TRUE, hyper = list(prec = list(initial = log(0.001), fixed = TRUE)))
 
 fit3 <- inla(formula3, data = df, family = "Poisson",
              control.predictor = list(link = 1),
              control.compute = list(config = TRUE))
 
-#' The first indexes for 15-19 are 1:4, 20-24 are 133:136 and 25-29 are 265:268
-#' (but for some reason the linear predictors are all the same)
+#' The first indexes for 15-19 are 1:4, 20-24 are 6601:6604 and 25-29 are 13201:13204
+
+min(which(df$age_idx == 2))
+min(which(df$age_idx == 3))
 
 calculate_pred <- function(fit) {
   etas_Y015_019 <- fit$summary.linear.predictor$mean[1:4]
-  etas_Y020_024 <-fit$summary.linear.predictor$mean[133:136]
-  etas_Y025_029 <-fit$summary.linear.predictor$mean[265:268]
+  etas_Y020_024 <-fit$summary.linear.predictor$mean[6601:6604]
+  etas_Y025_029 <-fit$summary.linear.predictor$mean[13201:13204]
 
   pred <- t(sapply(
     list(etas_Y015_019, etas_Y020_024, etas_Y025_029),
@@ -103,26 +104,31 @@ fit3$summary.random$cat_idx
 fit2$summary.random$cat_idx
 
 #' The age specific random effects are approximately zero
-#' Don't know why this is. Identifiability issues?
 fit3$summary.random$age_idx
 
-#' The empirical probabilities are as follows
-#' So it's not a problem wih the simulated data
+#' The empirical probabilities show it's not a problem with the simulated data
 df %>%
   group_by(age_group, cat_idx) %>%
   summarise(y_mean = mean(y))
 
-#' Model 4.
-#' Trying age group as a covariate (?)
-#' Doesn't seem to be working
+#' The problem is that
+#' exp(eta + c) / sum(exp(eta + c)) = exp(eta) / exp
+#' For example
+exp(etas) / sum(exp(etas))
+exp(etas + 1) / sum(exp(etas + 1))
 
-formula4 <- y ~ -1 + f(obs_idx, initial = -10, fixed = TRUE) +
-  f(cat_idx, age_idx, model = "iid", fixed = TRUE, constr = TRUE)
+#' Model 4.
+#' Age x category interactions rather than age random effects
+
+df <- mutate(df, age_cat_idx = interaction(age_idx, cat_idx))
+
+formula4 <- y ~ -1 + f(obs_idx, hyper = list(prec = list(initial = log(0.000001), fixed = TRUE))) +
+  f(cat_idx, model = "iid", constr = TRUE, hyper = list(prec = list(initial = log(0.001), fixed = TRUE))) +
+  f(age_cat_idx, model = "iid", constr = TRUE, hyper = list(prec = list(initial = log(0.001), fixed = TRUE)))
 
 fit4 <- inla(formula4, data = df, family = "Poisson",
              control.predictor = list(link = 1),
              control.compute = list(config = TRUE))
 
-fit4$summary.random$cat_idx
-fit4$summary.linear.predictor
-calculate_pred(fit4)
+#' Got it!
+list(truth = prob, pred = calculate_pred(fit4))
