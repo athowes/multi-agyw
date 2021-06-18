4f2936642626f39a46c66244137a01ec2a8c552e
Adam Howes <adamhowes@outlook.com>
2021-06-18 17:08:45 +0100

    Fit individual level model to data

--- src/fit_multi-sexbehav-sae/script.R
+++ src/fit_multi-sexbehav-sae/script.R
@@ -81,18 +81,37 @@ complete <- complete_eversex
 #' This considers each individual as a single multinomial trial
 #' It may be easier to get the weight into the likelihood but would take longer time to fit
 data_indiv <- complete %>%
-	filter(sex == "female", age <= 30) %>%
+	filter(sex == "female", age < 30) %>%
 	mutate(age_idx = findInterval(age, c(15, 20, 25, 30))) %>%
 	select(-survey_id, -individual_id, -indweight, -sex, -age) %>%
 	mutate(obs_idx = 1:n()) %>%
-	pivot_longer(nosex:sexpaid12m, values_to = "y", names_to = "cat_idx") %>%
-	mutate(cat_idx = as.integer(as.factor(cat_idx)))
+	pivot_longer(sexcohab:nosex, values_to = "y", names_to = "cat_idx") %>%
+	mutate(cat_idx = as.integer(as.factor(cat_idx)),
+	       age_cat_idx = interaction(age_idx, cat_idx))
+
+#' Try fitting a model using the individual level data
+tau_prior <- function(x) list(prec = list(initial = log(x), fixed = TRUE))
+
+formula <- y ~ -1 + f(obs_idx, hyper = tau_prior(0.000001)) +
+  f(cat_idx, model = "iid", constr = TRUE, hyper = tau_prior(0.001)) +
+  f(age_cat_idx, model = "iid", constr = TRUE, hyper = tau_prior(0.001))
+
+fit_indiv <- inla(formula, data = data_indiv, family = "Poisson",
+                  control.predictor = list(link = 1),
+                  control.compute = list(config = TRUE))
+
+data_indiv$eta <- fit_indiv$summary.linear.predictor$mean
+
+#' Compute probabilities
+#' TODO: Group by and filter to see the age_idx and cat_idx breakdown
+prob <- zoo::rollapply(data_indiv$eta, 4, by = 4, function(x) exp(x) / sum(exp(x)), partial = TRUE, align = "left")
+data_indiv$prob <- as.vector(t(prob))
 
 #' This considers each combination of age and cluster_id as a multinomial trial of n_i
 #' TODO: How to treat weights, n_eff_kish?
-#' TODO: Map cluster_id to regions of interests
+#' TODO: Map cluster_id to regions of interest
 data_aggregate <- complete %>%
-  filter(sex == 'female', age <= 30) %>%
+  filter(sex == 'female', age < 30) %>%
 	mutate(age_idx = findInterval(age, c(15, 20, 25, 30))) %>%
   select(-survey_id, -individual_id, -indweight, -sex, -age) %>%
 	group_by(cluster_id, age_idx) %>%
@@ -103,19 +122,20 @@ data_aggregate <- complete %>%
 	ungroup() %>%
   mutate(obs_idx = 1:n()) %>%
 	pivot_longer(nosex:sexpaid12m, values_to = "y", names_to = "cat_idx") %>%
-	mutate(cat_idx = as.integer(as.factor(cat_idx)))
+  mutate(cat_idx = as.integer(as.factor(cat_idx)),
+         age_cat_idx = interaction(age_idx, cat_idx))
 
-#' Test a model, using the Poisson trick
-formula <- y ~ -1 +
-	f(obs_idx, fixed = TRUE) +
-	f(cat_idx, age_idx, fixed = TRUE, constr = TRUE)
-	#' f(cluster_id, model = "besag", graph = G, constr = T)
-	#' TODO: make graph for id cluster
-
-fit <- inla(formula, data = data_aggregate, family = 'Poisson',
-					 control.predictor = list(link = 1), control.compute = list(config = TRUE))
-
-#' TODO: get prediction of prob.
-fit$summary.fitted %>% str
-fit$summary.random$cat_idx$mean
-fit$summary.random$obs_idx$mean %>% plot
+#' #' Test a model, using the Poisson trick
+#' formula <- y ~ -1 +
+#' 	f(obs_idx, fixed = TRUE) +
+#' 	f(cat_idx, age_idx, fixed = TRUE, constr = TRUE)
+#' 	#' f(cluster_id, model = "besag", graph = G, constr = T)
+#' 	#' TODO: make graph for id cluster
+#'
+#' fit_aggregate <- inla(formula, data = data_aggregate, family = 'Poisson',
+#' 					            control.predictor = list(link = 1), control.compute = list(config = TRUE))
+#'
+#' #' TODO: get prediction of prob.
+#' fit_aggregate$summary.fitted %>% str
+#' fit_aggregate$summary.random$cat_idx$mean
+#' fit_aggregate$summary.random$obs_idx$mean %>% plot
