f22a34431fc6662bdd1672b1798c910461a6216f
Adam Howes <adamhowes@outlook.com>
2021-06-22 14:15:05 +0100

    Analysis of the NA in the data, moving to use nosex12m rather than nosex as this has less NA problems and more complete case

--- src/fit_multi-sexbehav-sae/script.R
+++ src/fit_multi-sexbehav-sae/script.R
@@ -6,42 +6,84 @@
 #' This can be installed by following:
 # remotes::install_github("mrc-ide/naomi.utils@sexbehav-vars")
 
+#' 1. Get and clean the data
+
 #' Get data
 mwi <- create_surveys_dhs("MWI")
 dhs <- create_individual_dhs(mwi)
 sexbehav <- create_sexbehav_dhs(mwi) # Requires loading rdhs first
 
 #' We use 4 categories, following Katie
-use_data <- sexbehav %>%
+#' Only considering AGYW (female and under 30)
+data <- sexbehav %>%
   as_tibble %>%
 	mutate(nosex12m = 1 - sex12m,
 	       nosex = 1 - eversex) %>%
   select(-sex12m, -eversex, -sti12m) %>%
   relocate(nosex, nosex12m, .after = individual_id) %>%
-	bind_cols(select(dhs, cluster_id, sex, age, indweight))
+	bind_cols(select(dhs, cluster_id, sex, age, indweight)) %>%
+  filter(sex == "female", age < 30) %>%
+  mutate(age_idx = findInterval(age, c(15, 20, 25, 30)))
 
-#' Check the empirical proportions in each
+#' Examine the data with NA values
+na_data <- data[!complete.cases(data), ]
+
+#' The proportion of the data that has missing values in one of the rows
+nrow(na_data) / nrow(data)
+
+#' Most of the NA entries are in sexpaid12m, with a minority in nosex
+is.na(na_data) %>% colSums()
+
+#' Which age categories have the NA entries?
+#' Mostly the 15-19
+data %>%
+  group_by(age_idx) %>%
+  summarise(
+    across(
+      nosex:sexpaid12m,
+      list(tot = ~ sum(is.na(.x)),
+           pro = ~ sum(is.na(.x)) / n())
+    )
+  ) %>%
+  mutate_all(round, 2)
+
+#' Tending to have NA values when nosex = 1 or nosex12m = 1 (or NA)
+data %>%
+  group_by(nosex, nosex12m) %>%
+  summarise(
+    across(
+      sexcohab:sexpaid12m,
+      list(tot = ~ sum(is.na(.x)),
+           pro = ~ sum(is.na(.x)) / n())
+    )
+  ) %>%
+  mutate_all(round, 2)
+
+#' When nosex = 1, set sexpaid12m to be 0
+data <- data %>%
+  mutate(sexpaid12m = ifelse(nosex12m == 1, 0, sexpaid12m))
+
+#' Remove remaining rows with NA values (complete-case analysis)
+use_data <- na.omit(data)
+
+#' Check the empirical proportions in each category
 use_data %>%
   select(nosex, nosex12m, sexcohab, sexnonreg, sexpaid12m) %>%
-  colMeans(na.rm = TRUE)
+  colMeans()
 
-#' Check for overlapping (in more that one category)
-
-#' First do this with eversex
+#' Check for overlapping (in more that one category) both with eversex and sex12m
 complete_eversex <- use_data %>%
 	mutate(r_sum = nosex + sexcohab + sexnonreg + sexpaid12m) %>%
 	filter(r_sum == 1) %>%
 	select(-r_sum, -nosex12m)
 
-# This is the proportion which doesn't have r_sum == 1
-nrow(complete_eversex) / nrow(use_data)
-
-#' And now with sex12m
 complete_sex12m <- use_data %>%
   mutate(r_sum = nosex12m + sexcohab + sexnonreg + sexpaid12m) %>%
   filter(r_sum == 1) %>%
   select(-r_sum, -nosex)
 
+# This is the proportion which has r_sum == 1
+nrow(complete_eversex) / nrow(use_data)
 nrow(complete_sex12m) / nrow(use_data)
 
 #' Study the incomplete rows
@@ -50,13 +92,14 @@ incomplete_eversex <- use_data %>%
   filter(r_sum != 1) %>%
   select(-nosex12m)
 
-#' All of them have an r_sum value of 2
+#' Not all of these have an r_sum value of 2
 summary(incomplete_eversex$r_sum)
 
-#' There is only joint occurrence of
+#' Joint occurrence of
+#' 0 in everything
 #' sexnonreg, sexpaid12m
 #' sexcohab, sexpaid12m
-#' which thankfully seems quite easy to solve
+#' Later two easy to solve, first less so
 incomplete_eversex %>%
   group_by(nosex, sexcohab, sexnonreg, sexpaid12m) %>%
   summarise(count = n())
@@ -67,42 +110,40 @@ incomplete_sex12m <- use_data %>%
   filter(r_sum != 1) %>%
   select(-nosex)
 
-#' All of them also have an r_sum value of 2
+#' All of them have an r_sum value of 2
 summary(incomplete_sex12m$r_sum)
 
 #' Here we have joint occurrence of
 #' sexnonreg, sexpaid12m
 #' sexcohab, sexpaid12m
-#' nosex12m, sexpaid12m
-#' which is a little more difficult (nosex12m and sexpaid12m!)
-#' but still quite simple
+#' only
 incomplete_sex12m %>%
   group_by(nosex12m, sexcohab, sexnonreg, sexpaid12m) %>%
   summarise(count = n())
 
 #' Fix data by setting all rows with have sexpaid12m to be in that category
-#' Use eversex for now
+#' Use sex12m, as I think this fits the data best as a multinomial category
 use_data <- use_data %>%
   mutate(nosex = ifelse(sexpaid12m == 1, 0, nosex),
          nosex12m = ifelse(sexpaid12m == 1, 0, nosex12m),
          sexcohab = ifelse(sexpaid12m == 1, 0, sexcohab),
          sexnonreg = ifelse(sexpaid12m == 1, 0, sexnonreg)) %>%
-  select(-nosex12m)
+  select(-nosex)
 
 #' Verify no overlap
 stopifnot(
-  mutate(use_data, r_sum = nosex + sexcohab + sexnonreg + sexpaid12m) %>%
+  mutate(use_data, r_sum = nosex12m + sexcohab + sexnonreg + sexpaid12m) %>%
     filter(r_sum != 1) %>% nrow() == 0
 )
+
+#' 2. Fit individual level model
 
 #' This considers each individual as a single multinomial trial
 #' It may be easier to get the weight into the likelihood but would take longer time to fit
 data_indiv <- use_data %>%
-	filter(sex == "female", age < 30) %>%
-	mutate(age_idx = findInterval(age, c(15, 20, 25, 30))) %>%
 	select(-survey_id, -individual_id, -indweight, -sex, -age) %>%
 	mutate(obs_idx = 1:n()) %>%
-	pivot_longer(nosex:sexpaid12m, values_to = "y", names_to = "cat_idx") %>%
+	pivot_longer(nosex12m:sexpaid12m, values_to = "y", names_to = "cat_idx") %>%
 	mutate(cat_idx = as.integer(as.factor(cat_idx)),
 	       age_cat_idx = interaction(age_idx, cat_idx))
 
@@ -134,31 +175,29 @@ pred <- data_indiv %>%
 
 #' These are the empirical probabilities from the data for each of the three age categories
 truth <- use_data %>%
-  filter(sex == "female", age < 30) %>%
-  mutate(age_idx = findInterval(age, c(15, 20, 25, 30))) %>%
   group_by(age_idx) %>%
-  summarise(prob_nosex = mean(nosex),
+  summarise(prob_nosex12m = mean(nosex12m),
             prob_sexcohab = mean(sexcohab),
             prob_sexnonreg = mean(sexnonreg),
             prob_sexpaid12m = mean(sexpaid12m))
 
 list(truth = truth, pred = pred)
 
+#' 3. Fit aggregate level model
+
 #' This considers each combination of age and cluster_id as a multinomial trial of n_i
 #' TODO: How to treat weights, n_eff_kish?
 #' TODO: Map cluster_id to regions of interest
 data_aggregate <- use_data %>%
-  filter(sex == 'female', age < 30) %>%
-	mutate(age_idx = findInterval(age, c(15, 20, 25, 30))) %>%
   select(-survey_id, -individual_id, -indweight, -sex, -age) %>%
 	group_by(cluster_id, age_idx) %>%
-	summarise(nosex = sum(nosex),
+	summarise(nosex12m = sum(nosex12m),
 						sexcohab = sum(sexcohab),
 						sexnonreg = sum(sexnonreg),
 						sexpaid12m = sum(sexpaid12m)) %>%
 	ungroup() %>%
   mutate(obs_idx = 1:n()) %>%
-	pivot_longer(nosex:sexpaid12m, values_to = "y", names_to = "cat_idx") %>%
+	pivot_longer(nosex12m:sexpaid12m, values_to = "y", names_to = "cat_idx") %>%
   mutate(cat_idx = as.integer(as.factor(cat_idx)),
          age_cat_idx = interaction(age_idx, cat_idx))
 
