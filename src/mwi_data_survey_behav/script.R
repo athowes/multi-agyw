1dd5d55446e25e3fcf196438033ad7f521ade3e4
Adam Howes <adamhowes@outlook.com>
2021-06-23 15:23:42 +0100

    Add more comments to mwi_data_survey_behav (still more to go)

--- src/mwi_data_survey_behav/script.R
+++ src/mwi_data_survey_behav/script.R
@@ -5,16 +5,22 @@
 #' ISO3 country code
 iso3 <- "MWI"
 
+#' The spread_areas function "spreads area hierarchy to wide format"
 areas <- read_sf("depends/mwi_areas.geojson")
 areas_wide <- spread_areas(areas)
 
-surveys <- create_surveys_dhs(iso3)
-surveys <- surveys %>% dplyr::filter(SurveyYear==max(SurveyYear))
+#' Filter all of the DHS with the corresponding iso3 code to the most recent survey
+#' and obtain the meta data
+surveys <- create_surveys_dhs(iso3) %>%
+  filter(SurveyYear == max(SurveyYear))
 survey_meta <- create_survey_meta_dhs(surveys)
 
+#' "Create survey region boundaries dataset from DHS spatial data repository"
 survey_region_boundaries <- create_survey_boundaries_dhs(surveys)
-surveys <- surveys_add_dhs_regvar(surveys, survey_region_boundaries)
 
+#' "Add REGVAR to surveys dataset"
+#' TODO: What's REGVAR? Here it's "shdist"
+surveys <- surveys_add_dhs_regvar(surveys, survey_region_boundaries)
 
 #' MW2015DHS HR dataset has four city REGCODES that do not appear in boundaries:
 #' * 107: mzuzu city     [MWI_5_08]
@@ -22,17 +28,19 @@ surveys <- surveys_add_dhs_regvar(surveys, survey_region_boundaries)
 #' * 314: zomba city     [MWI_5_25]
 #' * 315: blantyre city  [MWI_5_33]
 #'
-#' Add this these to survey_region_boundaries dataset and update district
-#' boundaries to exclude cities
-
+#' Add these to survey_region_boundaries and update district boundaries to exclude cities
+#' From https://dhsprogram.com/data/File-Types-and-Names.cfm:
+#' * HR stands for "Household Recode"
+#' * FL stands for "Flat (ASCII) files"
 hrd <- dhs_datasets(surveyIds = "MW2015DHS", fileType = "HR", fileFormat = "FL")
-hr <- readRDS(get_datasets(hrd)[[1]])
+hr <- readRDS(get_datasets(hrd)[[1]]) #' There is only one survey here
 
 hr15dhs_regions <- hr %>%
   distinct(survey_id = "MWI2015DHS",
            survey_region_id = shdist,
            survey_region_name = as_factor(shdist))
 
+#' All of the rows of hr15dhs_regions without a match in survey_region_boundaries (107, 210, 314, 315)
 anti_join(hr15dhs_regions,
           survey_region_boundaries,
           by = c("survey_id", "survey_region_id"))
@@ -61,8 +69,6 @@ metro_boundaries <- metro_boundaries %>%
   ) %>%
   select(names(survey_region_boundaries))
 
-
-
 regspl <- split(survey_region_boundaries, survey_region_boundaries$survey_id)
 
 #' Remove metro boundaries from 28 districts
@@ -72,7 +78,6 @@ regspl$MWI2015DHS <- regspl$MWI2015DHS %>%
   )
 
 #' Append metros to MWI2015DHS survey regions
-
 regspl$MWI2015DHS <- bind_rows(regspl$MWI2015DHS, metro_boundaries)
 
 survey_region_boundaries <- bind_rows(regspl)
@@ -85,11 +90,9 @@ stopifnot(
   ) == 0
 )
 
-
 #' Allocate each area to survey region
 survey_region_areas <- allocate_areas_survey_regions(areas_wide, survey_region_boundaries)
 validate_survey_region_areas(survey_region_areas, survey_region_boundaries)
-
 
 survey_regions <- create_survey_regions_dhs(survey_region_areas)
 
@@ -97,7 +100,6 @@ survey_regions <- create_survey_regions_dhs(survey_region_areas)
 survey_clusters <- create_survey_clusters_dhs(surveys)
 
 #' Snap survey clusters to areas
-
 survey_clusters <- assign_dhs_cluster_areas(survey_clusters, survey_region_areas)
 
 p_coord_check <- plot_survey_coordinate_check(survey_clusters,
@@ -110,20 +112,17 @@ p_coord_check
 dev.off()
 
 #' # Individual dataset
-
 individuals <- create_individual_hiv_dhs(surveys)
 
 survey_individuals <- create_survey_individuals_dhs(individuals)
 survey_biomarker <- create_survey_biomarker_dhs(individuals)
-
 survey_sexbehav <- create_sexbehav_dhs(surveys)
 survey_other <- list(survey_sexbehav)
 
-age_group_include <- c("Y015_019","Y020_024", "Y025_029","Y015_024")
+age_group_include <- c("Y015_019", "Y020_024", "Y025_029","Y015_024")
 sex <- c("female")
 
-#' # Survey indicator dataset
-
+#' Survey indicator dataset
 survey_indicators <- calc_survey_indicators(
   survey_meta,
   survey_regions,
@@ -137,12 +136,10 @@ survey_indicators <- calc_survey_indicators(
   area_bottom_level = 5
 )
 
-#' ## Save survey indicators dataset
+#' Save survey indicators dataset
 write_csv(survey_indicators, "mwi_survey_indicators_sexbehav.csv", na = "")
 
-
-#' ## Get prevalence estimates for different sexual behaviours
-
+#' Get prevalence estimates for different sexual behaviours
 hiv_indicators <- calc_survey_hiv_indicators(
   survey_meta,
   survey_regions,
@@ -159,8 +156,7 @@ hiv_indicators <- calc_survey_hiv_indicators(
     sex12m + eversex + sexcohab + sexnonreg + sexpaid12m + sti12m
 )
 
-#' ## Keep only the stratifications w/ "all" for others
-
+#' Keep only the stratifications with "all" for others
 hiv_indicators <- dplyr::bind_rows(dplyr::filter(hiv_indicators, eversex=="all" & sexcohab=="all" &
                                          sexnonreg=="all" & sexpaid12m=="all" & sti12m=="all" &
                                          (!is.na(sex12m) & sex12m!="all")),
@@ -180,5 +176,5 @@ hiv_indicators <- dplyr::bind_rows(dplyr::filter(hiv_indicators, eversex=="all" 
                                          sexcohab=="all" & sexnonreg=="all" & sexpaid12m=="all" &
                                        (!is.na(sti12m) & sti12m!="all")))
 
-#' ## Save HIV indicators dataset
+#' Save HIV indicators dataset
 write_csv(hiv_indicators, "mwi_hiv_indicators_sexbehav.csv", na = "")
