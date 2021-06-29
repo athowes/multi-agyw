08f842cec4c95718b4cab927d2d16e18f45cf69b
Adam Howes <adamhowes@outlook.com>
2021-06-29 11:03:20 +0100

    Update check that estimates sum to 100% with multinomial results

--- src/check100_sexbehav-sae/script.R
+++ src/check100_sexbehav-sae/script.R
@@ -2,42 +2,80 @@
 # orderly::orderly_develop_start("check100_sexbehav-sae")
 # setwd("src/check100_sexbehav-sae/")
 
-df <- read_csv("depends/smoothed-district-sexbehav.csv")
+#' Trying to determine if the estimates sum to 100%
+df_uni <- read_csv("depends/smoothed-district-sexbehav.csv") %>%
+  select(indicator, age_group, area_name, area_idx, mean)
 
-df <- select(df, indicator, age_group, area_name, area_idx, estimate)
+#' Add rows for nosex12m and neversex indicators
+df_uni <- bind_rows(
+  df_uni,
+  #' Add nosex12m indicator (1 - sex12m)
+  df_uni %>%
+    filter(indicator == "sex12m") %>%
+    mutate(mean = 1 - mean,
+           indicator = "nosex12m"),
+  #' Add neversex indicator (1 - eversex)
+  df_uni %>%
+    filter(indicator == "eversex") %>%
+    mutate(mean = 1 - mean,
+           indicator = "neversex")
+) %>%
+  mutate(type = "univariate",
+         model = "Model 1")
+
+df_mul <- read_csv("depends/multinomial-smoothed-district-sexbehav.csv") %>%
+  select(indicator, age_group, area_name, area_idx, mean, model) %>%
+  mutate(type = "multivariate")
+
+df <- rbind(df_uni, df_mul) %>%
+  mutate(
+    age_group = fct_relevel(age_group, "Y015_024") %>%
+      fct_recode("15-24" = "Y015_024", "15-19" = "Y015_019", "20-24" = "Y020_024", "25-29" = "Y025_029")
+  )
 
 stacked_bar <- function(df) {
-  ggplot(df, aes(fill = indicator, y = estimate, x = area_name)) +
+  ggplot(df, aes(fill = indicator, y = mean, x = area_name)) +
     geom_bar(position = "stack", stat = "identity") +
     facet_wrap(~age_group) +
     geom_hline(yintercept = 1, linetype = "dashed") +
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+    scale_y_continuous(labels = scales::percent) +
+    labs(title = paste0("Estimates: ", df$type[1], ", ", tolower(df$model[1]))) +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+          legend.position = "bottom",
+          legend.key.width = unit(2, "lines"))
 }
 
 pdf("check100.pdf", h = 11, w = 8.5)
 
-stacked_bar(df)
-
-df <- bind_rows(
-  df,
-  #' Add nosex12m indicator (1 - sex12m)
-  df %>%
-    filter(indicator == "sex12m") %>%
-    mutate(estimate = 1 - estimate,
-           indicator = "nosex12m"),
-  #' Add neversex indicator (1 - eversex)
-  df %>%
-    filter(indicator == "eversex") %>%
-    mutate(estimate = 1 - estimate,
-           indicator = "neversex")
-)
-
+#' Univariate smoothed using neversex
 df %>%
-  filter(!(indicator %in%  c("sti12m", "eversex", "sex12m", "nosex12m"))) %>%
+  filter(type == "univariate",
+         indicator %in%  c("neversex", "sexcohab", "sexnonreg", "sexpaid12m")) %>%
   stacked_bar()
 
+#' Univariate smoothed using nosex12m
 df %>%
-  filter(!(indicator %in%  c("sti12m", "eversex", "sex12m", "neversex"))) %>%
+  filter(type == "univariate",
+         indicator %in%  c("nosex12m", "sexcohab", "sexnonreg", "sexpaid12m")) %>%
+  stacked_bar()
+
+#' Multivariate smoothed with Model 1
+df %>%
+  filter(type == "multivariate",
+         model == "Model 1") %>%
+  stacked_bar()
+
+#' Multivariate smoothed with Model 2
+df %>%
+  filter(type == "multivariate",
+         model == "Model 2") %>%
+  stacked_bar()
+
+#' Multivariate smoothed with Model 3
+#' TODO: Could make this lapply rather than typing it out manually
+df %>%
+  filter(type == "multivariate",
+         model == "Model 3") %>%
   stacked_bar()
 
 dev.off()
