68c389b24b8de0ee93fcdc1463a5acfdb8903ace
Adam Howes <adamhowes@outlook.com>
2021-07-01 18:07:25 +0100

    Script to run, commit and push to archive the data prep reports (aaa_data_survey_behav)

---
+++ utils/run_data_prep.R
@@ -0,0 +1,30 @@
+#' The 13 GF AGYW countries minus BWA
+iso3 <- c("CMR", "KEN", "LSO", "MOZ", "MWI", "NAM", "SWZ", "TZA", "UGA", "ZAF", "ZMB", "ZWE")
+
+#' The names of the reports to run
+# reports <- paste0(tolower(iso3), "_data_survey_behav")
+
+#' Just try doing the one for now
+reports <- "cmr_data_survey_behav"
+
+#' Try to run them
+sapply(
+  reports,
+  function(report) {
+    #' Run the report
+    tryCatch(
+      id <- orderly::orderly_run(report),
+      error = function(e) paste0("Report ", report, " failed to run.")
+    )
+    #' Commit it to archive
+    tryCatch(
+      orderly::orderly_commit(id),
+      error = function(e) paste0("Report ", report, " failed to commit.")
+    )
+    #' Push archive to remote
+    tryCatch(
+      orderly::orderly_push_archive(report),
+      error = function(e) paste0("Report ", report, " could not be pushed to the remote.")
+    )
+  }
+)
