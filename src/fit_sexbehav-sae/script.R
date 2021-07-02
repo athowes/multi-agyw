ec4498c6c17d2c7e38ef7686289f438f41a96fa7
Adam Howes <adamhowes@outlook.com>
2021-07-02 12:57:54 +0100

    fit_multi-sexbehav-sae and fit_sexbehav-sae for more iso3. Note: requires running all of the aaa_data_survey_behav and commiting to archive before running now

--- src/fit_sexbehav-sae/script.R
+++ src/fit_sexbehav-sae/script.R
@@ -2,8 +2,31 @@
 # orderly::orderly_develop_start("fit_sexbehav-sae")
 # setwd("src/fit_sexbehav-sae")
 
-analysis_level <- c("MWI" = 5)
-admin1_level <- c("MWI" = 1)
+analysis_level <- c("CMR" = 2,
+                    "KEN" = 2,
+                    "LSO" = 1,
+                    "MOZ" = 2,
+                    "MWI" = 5,
+                    "NAM" = 2,
+                    "SWZ" = 1,
+                    "TZA" = 3,
+                    "UGA" = 3,
+                    "ZAF" = 2,
+                    "ZMB" = 2,
+                    "ZWE" = 2)
+
+admin1_level <- c("CMR" = 1,
+                  "KEN" = 1,
+                  "LSO" = 1,
+                  "MOZ" = 1,
+                  "MWI" = 1,
+                  "NAM" = 1,
+                  "SWZ" = 1,
+                  "TZA" = 2,
+                  "UGA" = 1,
+                  "ZAF" = 1,
+                  "ZMB" = 1,
+                  "ZWE" = 1)
 
 stopifnot(iso3 %in% names(analysis_level))
 stopifnot(iso3 %in% names(admin1_level))
