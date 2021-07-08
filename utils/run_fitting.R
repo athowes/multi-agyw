d86509bd179f9a4e8508abf9c4030d9efa0ae245
Adam Howes <adamhowes@outlook.com>
2021-07-08 15:50:15 +0100

    Commiting and pushing

--- utils/run_fitting.R
+++ utils/run_fitting.R
@@ -1,5 +1,6 @@
 #' Run everything in bulk
-params <- data.frame(iso3 = c("CMR", "KEN", "LSO", "MOZ", "MWI", "NAM", "SWZ", "TZA", "UGA", "ZAF", "ZMB", "ZWE"))
+iso3_vec <- c("CMR", "KEN", "LSO", "MOZ", "MWI", "NAM", "SWZ", "TZA", "UGA", "ZAF", "ZMB", "ZWE")
+params <- data.frame(iso3 = iso3_vec)
 ids <- orderly::orderly_batch("aaa_fit_multi-sexbehav-sae", params)
 
 #' Running individually
@@ -15,3 +16,18 @@ ids <- orderly::orderly_batch("aaa_fit_multi-sexbehav-sae", params)
 # id <- orderly::orderly_run("aaa_fit_multi-sexbehav-sae", parameters = list(iso3 = "ZAF"))
 # id <- orderly::orderly_run("aaa_fit_multi-sexbehav-sae", parameters = list(iso3 = "ZMB"))
 # id <- orderly::orderly_run("aaa_fit_multi-sexbehav-sae", parameters = list(iso3 = "ZWE"))
+
+#' Get the most recent archived or drafted version of each of the reports
+#' Could also use ids from orderly_batch here
+recent_ids <- sapply(
+  iso3_vec,
+  function(iso3) {
+    orderly::orderly_search(query = paste0("latest(parameter:iso3 == '", iso3, "')"), "aaa_fit_multi-sexbehav-sae", draft = TRUE)
+  }
+)
+
+#' Commit them (if not already in the archive?)
+sapply(na.omit(recent_ids), function(id) orderly::orderly_commit(id))
+
+#' Push to remote
+sapply(na.omit(recent_ids), function(id) orderly::orderly_push_archive(name = "aaa_fit_multi-sexbehav-sae", id = id))
