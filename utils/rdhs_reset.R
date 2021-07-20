7a7b0ad4d5e7e8024d98a3733b138dc6bbe03393
Adam Howes <adamhowes@outlook.com>
2021-07-20 20:13:15 +0100

    Write this often enough that it's better in utils/

---
+++ utils/rdhs_reset.R
@@ -0,0 +1,3 @@
+#' A command for when rdhs is running into problems and the cache has to be cleared
+library(rdhs)
+rdhs::set_rdhs_config(email = "ath19@ic.ac.uk", project = "Statistics and Machine Learning for HIV", password_prompt = TRUE)
