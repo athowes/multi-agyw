c5e518d3be75d002c7ed6c6aa5b3e08f8f0feec7
Adam Howes <adamhowes@outlook.com>
2021-08-02 15:16:55 +0100

    Rename run_fitting and add a run script for the all-dhs spatio-temporal SAE

---
+++ utils/run_fit_all-dhs-multi-sexbehav-sae.R
@@ -0,0 +1,20 @@
+#' Run fit_all-dhs-multi-sexbehav-sae in bulk
+
+# iso3_vec <- c("CMR", "KEN", "LSO", "MOZ", "MWI", "NAM", "SWZ", "TZA", "UGA", "ZAF", "ZMB", "ZWE")
+iso3_vec <- c("CMR", "MOZ", "MWI", "NAM", "SWZ", "ZAF", "ZMB", "ZWE")
+
+params <- data.frame(iso3 = iso3_vec)
+ids <- orderly::orderly_batch("aaa_fit_all-dhs-multi-sexbehav-sae", params)
+
+recent_ids <- sapply(
+  iso3_vec,
+  function(iso3) {
+    orderly::orderly_search(query = paste0("latest(parameter:iso3 == '", iso3, "')"), "aaa_fit_all-dhs-multi-sexbehav-sae", draft = TRUE)
+  }
+)
+
+#' Commit them (if not already in the archive?)
+sapply(na.omit(recent_ids), function(id) orderly::orderly_commit(id))
+
+#' Push to remote
+sapply(na.omit(recent_ids), function(id) orderly::orderly_push_archive(name = "aaa_fit_all-dhs-multi-sexbehav-sae", id = id))
---
+++ utils/run_fit_multi-sexbehav-sae.R
@@ -0,0 +1,41 @@
+#' Run fit_multi-sexbehav-sae in bulk
+
+iso3_vec <- c("CMR", "KEN", "LSO", "MOZ", "MWI", "NAM", "SWZ", "TZA", "UGA", "ZAF", "ZMB", "ZWE")
+params <- data.frame(iso3 = iso3_vec)
+ids <- orderly::orderly_batch("aaa_fit_multi-sexbehav-sae", params)
+
+#' Running individually
+# id <- orderly::orderly_run("aaa_fit_multi-sexbehav-sae", parameters = list(iso3 = "CMR"))
+# id <- orderly::orderly_run("aaa_fit_multi-sexbehav-sae", parameters = list(iso3 = "KEN"))
+# id <- orderly::orderly_run("aaa_fit_multi-sexbehav-sae", parameters = list(iso3 = "LSO"))
+# id <- orderly::orderly_run("aaa_fit_multi-sexbehav-sae", parameters = list(iso3 = "MOZ"))
+# id <- orderly::orderly_run("aaa_fit_multi-sexbehav-sae", parameters = list(iso3 = "MWI"))
+# id <- orderly::orderly_run("aaa_fit_multi-sexbehav-sae", parameters = list(iso3 = "NAM"))
+# id <- orderly::orderly_run("aaa_fit_multi-sexbehav-sae", parameters = list(iso3 = "SWZ"))
+# id <- orderly::orderly_run("aaa_fit_multi-sexbehav-sae", parameters = list(iso3 = "TZA"))
+# id <- orderly::orderly_run("aaa_fit_multi-sexbehav-sae", parameters = list(iso3 = "UGA"))
+# id <- orderly::orderly_run("aaa_fit_multi-sexbehav-sae", parameters = list(iso3 = "ZAF"))
+# id <- orderly::orderly_run("aaa_fit_multi-sexbehav-sae", parameters = list(iso3 = "ZMB"))
+# id <- orderly::orderly_run("aaa_fit_multi-sexbehav-sae", parameters = list(iso3 = "ZWE"))
+
+#' Get the most recent archived or drafted version of each of the reports
+#' Could also use ids from orderly_batch here, but if you've just run it then they should be the same
+recent_ids <- sapply(
+  iso3_vec,
+  function(iso3) {
+    orderly::orderly_search(query = paste0("latest(parameter:iso3 == '", iso3, "')"), "aaa_fit_multi-sexbehav-sae", draft = TRUE)
+  }
+)
+
+#' Commit them (if not already in the archive?)
+sapply(na.omit(recent_ids), function(id) orderly::orderly_commit(id))
+
+#' Push to remote
+sapply(na.omit(recent_ids), function(id) orderly::orderly_push_archive(name = "aaa_fit_multi-sexbehav-sae", id = id))
+
+#' Run post-processing as well
+id <- orderly::orderly_run("src/process_information-criteria/")
+orderly::orderly_commit(id)
+
+id <- orderly::orderly_run("src/process_multinomial-smoothed-district-sexbehav/")
+orderly::orderly_commit(id)
