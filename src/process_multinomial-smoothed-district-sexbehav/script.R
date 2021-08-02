5528b79e56d440247556207681dd45994ecd6ba6
Adam Howes <adamhowes@outlook.com>
2021-08-02 15:17:19 +0100

    Add all-dhs estimates

--- src/process_multinomial-smoothed-district-sexbehav/script.R
+++ src/process_multinomial-smoothed-district-sexbehav/script.R
@@ -1,10 +1,17 @@
 #' Uncomment and run the two line below to resume development of this script
-# orderly::orderly_develop_start("process_multinomial-smoothed-district-sexbehav")
+orderly::orderly_develop_start("process_multinomial-smoothed-district-sexbehav")
 # setwd("src/process_multinomial-smoothed-district-sexbehav")
 
 iso3 <- c("CMR", "KEN", "LSO", "MOZ", "MWI", "NAM", "SWZ", "TZA", "UGA", "ZAF", "ZMB", "ZWE")
-files <- paste0("depends/", tolower(iso3), "_multinomial-smoothed-district-sexbehav.csv")
 
+#' The single survey estimates
+files <- paste0("depends/", tolower(iso3), "_multinomial-smoothed-district-sexbehav.csv")
 df <- bind_rows(lapply(files, function(file) read_csv(file)))
 
-write_csv(df, "all-multinomial-smoothed-district-sexbehav.csv", na = "")
+write_csv(df, "every-multinomial-smoothed-district-sexbehav.csv", na = "")
+
+#' The multiple survey estimates
+files <- paste0("depends/", tolower(iso3), "_all-dhs-multinomial-smoothed-district-sexbehav.csv")
+df <- bind_rows(lapply(files, function(file) read_csv(file)))
+
+write_csv(df, "every-all-dhs-multinomial-smoothed-district-sexbehav.csv", na = "")
