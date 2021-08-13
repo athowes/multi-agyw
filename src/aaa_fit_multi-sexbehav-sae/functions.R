875c74c0428b32feedb10fd9a632fb5abecc998f
Adam Howes <adamhowes@outlook.com>
2021-08-13 16:36:22 +0100

    Adds #014 for the single DHS script

--- src/aaa_fit_multi-sexbehav-sae/functions.R
+++ src/aaa_fit_multi-sexbehav-sae/functions.R
@@ -48,9 +48,7 @@ multinomial_model <- function(formula, model_name, S = 100) {
   df <- df %>%
     mutate(
       #' Add mean of linear predictor
-      eta = fit$summary.linear.predictor$mean,
-      #' Mean of fitted value
-      lambda = fit$summary.fitted.values$mean
+      eta = fit$summary.linear.predictor$mean
     ) %>%
     #' Split by observation indicator and lapply softmax
     split(.$obs_idx) %>%
@@ -90,7 +88,12 @@ multinomial_model <- function(formula, model_name, S = 100) {
           mutate(prob = stable_softmax(eta))
       ) %>%
       bind_rows() %>%
-      mutate(sample = i)
+      mutate(
+        #' Sample of the intensity
+        lambda = exp(eta),
+        #' Sample number / identifier
+        sample = i
+      )
   ) %>%
     bind_rows()
 
@@ -102,6 +105,9 @@ multinomial_model <- function(formula, model_name, S = 100) {
         summarise(median = quantile(prob, 0.5, na.rm = TRUE),
                   lower = quantile(prob, 0.025, na.rm = TRUE),
                   upper = quantile(prob, 0.975, na.rm = TRUE),
+                  lambda_median = quantile(lambda, 0.5, na.rm = TRUE),
+                  lambda_lower = quantile(lambda, 0.025, na.rm = TRUE),
+                  lambda_upper = quantile(lambda, 0.975, na.rm = TRUE),
                   .groups = "drop"),
       by = c("obs_idx", "cat_idx")
     )
