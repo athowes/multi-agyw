33e65398568f7213c2983a2a0898a39305ce26c9
Adam Howes <adamhowes@outlook.com>
2021-08-14 21:19:23 +0100

    Log of the current (sad) state of model fitting

--- utils/run_fit_all-dhs-multi-sexbehav-sae.R
+++ utils/run_fit_all-dhs-multi-sexbehav-sae.R
@@ -1,10 +1,58 @@
 #' Run fit_all-dhs-multi-sexbehav-sae in bulk
 
-# iso3_vec <- c("CMR", "KEN", "LSO", "MOZ", "MWI", "NAM", "SWZ", "TZA", "UGA", "ZAF", "ZMB", "ZWE")
-iso3_vec <- c("CMR", "MOZ", "MWI", "NAM", "SWZ", "ZAF", "ZMB", "ZWE")
+iso3_vec <- c("CMR", "KEN", "LSO", "MOZ", "MWI", "NAM", "SWZ", "TZA", "UGA", "ZAF", "ZMB", "ZWE")
 
 params <- data.frame(iso3 = iso3_vec)
 ids <- orderly::orderly_batch("aaa_fit_all-dhs-multi-sexbehav-sae", params)
+
+#' Running individually
+#' ##: the models have successfully run
+#' #: the models have not yet successfully run
+
+## id <- orderly::orderly_run("aaa_fit_all-dhs-multi-sexbehav-sae", parameters = list(iso3 = "CMR"))
+#' Issue with not having population data for CMR? The aggregate is not working here for some reason
+
+# id <- orderly::orderly_run("aaa_fit_all-dhs-multi-sexbehav-sae", parameters = list(iso3 = "KEN"))
+#' Data creation issue #009
+#' Begin fitting Model 7.
+#' inla.mkl: src/inla.c:23905: inla_parse_ffield: Assertion `def->n > 1' failed.
+
+# id <- orderly::orderly_run("aaa_fit_all-dhs-multi-sexbehav-sae", parameters = list(iso3 = "LSO"))
+#' Data creation issue #009
+#' Begin fitting Model 7.
+#' inla.mkl: src/inla.c:23905: inla_parse_ffield: Assertion `def->n > 1' failed.
+
+## id <- orderly::orderly_run("aaa_fit_all-dhs-multi-sexbehav-sae", parameters = list(iso3 = "MOZ"))
+
+## id <- orderly::orderly_run("aaa_fit_all-dhs-multi-sexbehav-sae", parameters = list(iso3 = "MWI"))
+
+# id <- orderly::orderly_run("aaa_fit_all-dhs-multi-sexbehav-sae", parameters = list(iso3 = "NAM"))
+#' Begin fitting Model 5.
+#' *** Warning *** Numerical error gives b22 = -8.79609e+12 <= 0.0 for idx=586. setting b22=1.49167e-154
+#' Begin fitting Model 7.
+#' inla.mkl: src/inla.c:23905: inla_parse_ffield: Assertion `def->n > 1' failed.
+
+# id <- orderly::orderly_run("aaa_fit_all-dhs-multi-sexbehav-sae", parameters = list(iso3 = "SWZ"))
+#' Begin fitting Model 7.
+#' inla.mkl: src/inla.c:23905: inla_parse_ffield: Assertion `def->n > 1' failed.
+
+# id <- orderly::orderly_run("aaa_fit_all-dhs-multi-sexbehav-sae", parameters = list(iso3 = "TZA"))
+#' Data creation issue #009
+#' Begin fitting Model 7.
+#' inla.mkl: src/inla.c:23905: inla_parse_ffield: Assertion `def->n > 1' failed.
+
+# id <- orderly::orderly_run("aaa_fit_all-dhs-multi-sexbehav-sae", parameters = list(iso3 = "UGA"))
+#' Data creation issue #009
+#' Begin fitting Model 7.
+#' inla.mkl: src/inla.c:23905: inla_parse_ffield: Assertion `def->n > 1' failed.
+
+# id <- orderly::orderly_run("aaa_fit_all-dhs-multi-sexbehav-sae", parameters = list(iso3 = "ZAF"))
+#' Begin fitting Model 7.
+#' inla.mkl: src/inla.c:23905: inla_parse_ffield: Assertion `def->n > 1' failed.
+
+## id <- orderly::orderly_run("aaa_fit_all-dhs-multi-sexbehav-sae", parameters = list(iso3 = "ZMB"))
+
+## id <- orderly::orderly_run("aaa_fit_all-dhs-multi-sexbehav-sae", parameters = list(iso3 = "ZWE"))
 
 recent_ids <- sapply(
   iso3_vec,
