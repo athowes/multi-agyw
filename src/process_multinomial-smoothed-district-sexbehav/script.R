2c5ad9e85c41b8cf8771cd505c836e40b4db37ff
Adam Howes <adamhowes@outlook.com>
2021-08-18 15:01:38 +0100

    Just the ones that worked so that I can continue development of plots e.g. #018, #019

--- src/process_multinomial-smoothed-district-sexbehav/script.R
+++ src/process_multinomial-smoothed-district-sexbehav/script.R
@@ -1,14 +1,16 @@
 #' Uncomment and run the two line below to resume development of this script
-orderly::orderly_develop_start("process_multinomial-smoothed-district-sexbehav")
+# orderly::orderly_develop_start("process_multinomial-smoothed-district-sexbehav")
 # setwd("src/process_multinomial-smoothed-district-sexbehav")
 
-iso3 <- c("CMR", "KEN", "LSO", "MOZ", "MWI", "NAM", "SWZ", "TZA", "UGA", "ZAF", "ZMB", "ZWE")
+iso3 <- c("BWA", "CMR", "KEN", "LSO", "MOZ", "MWI", "NAM", "SWZ", "TZA", "UGA", "ZAF", "ZMB", "ZWE")
 
 #' The single survey estimates
 files <- paste0("depends/", tolower(iso3), "_multinomial-smoothed-district-sexbehav.csv")
 df <- bind_rows(lapply(files, function(file) read_csv(file)))
 
 write_csv(df, "every-multinomial-smoothed-district-sexbehav.csv", na = "")
+
+iso3 <- c("CMR", "MOZ", "MWI", "ZMB", "ZWE")
 
 #' The multiple survey estimates
 files <- paste0("depends/", tolower(iso3), "_all-dhs-multinomial-smoothed-district-sexbehav.csv")
