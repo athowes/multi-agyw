c6a82574a9f91e0c548c47ebbd0a96080883691b
Adam Howes <adamhowes@outlook.com>
2021-08-20 23:57:54 +0100

    Generalise dic_plot to ic_plot for CPO and PIT. Still a bug for getting the colouring of best model correct. Progress towards #024

--- src/process_information-criteria/script.R
+++ src/process_information-criteria/script.R
@@ -11,46 +11,62 @@ df <- bind_rows(lapply(files, function(file) read_csv(file)))
 
 write_csv(df, "model-comparison.csv", na = "")
 
-df <- df %>%
-  # #' Could clean the DIC for infeasible values here
-  # mutate(dic = ifelse(abs(dic) > 10^5, NA, dic)) %>%
-  #' Set names for plotting
-  mutate(model =
-           fct_recode(model,
-             "1" = "Model 1: Constant",
-             "2" = "Model 2: IID",
-             "3" = "Model 3: BYM2"
-            )
-  ) %>%
-  group_by(iso3) %>%
-  #' Add best performing model indicator
-  mutate(min_idx = (min(dic, na.rm = TRUE) == dic))
+ic_plot <- function(df, ic = "dic", minimum = TRUE) {
 
-dic_plot <- function(df) {
-  ggplot(df, aes(x = model,
-                 y = dic,
-                 ymin = dic - 1.96 * dic_se,
-                 ymax = dic + 1.96 * dic_se,
-                 col = min_idx)) +
-    geom_pointrange(alpha = 0.7) +
-    facet_wrap(~iso3, scales = "free") +
-    scale_color_manual(values = c("black", "#E69F00")) +
-    labs(x = "", y = "DIC",
-         title = paste0("DIC results for the models in ", length(iso3), " countries"),
-         subtitle = "Missing entries indicate that the DIC value returned was NA.",
-         col = "Amongst best model(s)") +
-    theme_minimal() +
-    theme(
-      strip.text = element_text(face = "bold"),
-      plot.title = element_text(face = "bold"),
-      legend.position = "bottom",
-      legend.key.width = unit(4, "lines")
-    )
+  ic <- sym(ic)
+  ic_se <- sym(paste0(ic, "_se"))
+
+  df %>%
+    # #' Could clean the DIC for infeasible values here
+    # mutate(dic = ifelse(abs(dic) > 10^5, NA, dic)) %>%
+    #' Set names for plotting
+    mutate(model =
+             fct_recode(model,
+                        "1" = "Model 1: Constant",
+                        "2" = "Model 2: IID",
+                        "3" = "Model 3: BYM2",
+                        "1" = "Model 1",
+                        "2" = "Model 2",
+                        "3" = "Model 3",
+                        "4" = "Model 4",
+                        "5" = "Model 5",
+                        "6" = "Model 6",
+                        "7" = "Model 7",
+                        "8" = "Model 8",
+                        "9" = "Model 9"
+             )
+    ) %>%
+    group_by(iso3) %>%
+    #' Add best performing model indicator (minimum and maximum)
+    mutate(
+      min_idx = (min(!!ic, na.rm = TRUE) == !!ic),
+      max_idx = (max(!!ic, na.rm = TRUE) == !!ic),
+      best_idx = ifelse(minimum, min_idx, max_idx)
+    ) %>%
+    ggplot(aes(x = model,
+               y = !!ic,
+               ymin = !!ic - 1.96 * !!ic_se,
+               ymax = !!ic + 1.96 * !!ic_se,
+               col = best_idx)) +
+      geom_pointrange(alpha = 0.7) +
+      facet_wrap(~iso3, scales = "free") +
+      scale_color_manual(values = c("black", "#E69F00")) +
+      labs(x = "", y = paste0(toupper(ic)),
+           title = paste0(toupper(ic), " results for the models in ", length(iso3), " countries"),
+           subtitle = "Missing entries indicate that the value returned was NA.",
+           col = "Amongst best model(s)") +
+      theme_minimal() +
+      theme(
+        strip.text = element_text(face = "bold"),
+        plot.title = element_text(face = "bold"),
+        legend.position = "bottom",
+        legend.key.width = unit(4, "lines")
+      )
 }
 
-pdf("model-comparison.pdf", h = 5, w = 8.5)
+pdf("dic-model-comparison.pdf", h = 5, w = 8.5)
 
-dic_plot(df)
+ic_plot(df)
 
 dev.off()
 
@@ -63,27 +79,20 @@ df <- bind_rows(lapply(files, function(file) read_csv(file)))
 
 write_csv(df, "all-dhs-model-comparison.csv", na = "")
 
-df <- df %>%
-  #' Set names for plotting
-  mutate(model =
-           fct_recode(model,
-                      "1" = "Model 1",
-                      "2" = "Model 2",
-                      "3" = "Model 3",
-                      "4" = "Model 4",
-                      "5" = "Model 5",
-                      "6" = "Model 6",
-                      "7" = "Model 7",
-                      "8" = "Model 8",
-                      "9" = "Model 9"
-           )
-  ) %>%
-  group_by(iso3) %>%
-  #' Add best performing model indicator
-  mutate(min_idx = (min(dic, na.rm = TRUE) == dic))
+pdf("all-dhs-dic-model-comparison.pdf", h = 5, w = 8.5)
 
-pdf("all-dhs-model-comparison.pdf", h = 5, w = 8.5)
+ic_plot(df, ic = "dic", min = TRUE)
 
-dic_plot(df)
+dev.off()
+
+pdf("all-dhs-cpo-model-comparison.pdf", h = 5, w = 8.5)
+
+ic_plot(df, ic = "cpo", min = FALSE)
+
+dev.off()
+
+pdf("all-dhs-pit-model-comparison.pdf", h = 5, w = 8.5)
+
+ic_plot(df, ic = "pit")
 
 dev.off()
