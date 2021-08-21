cccbca5f3b4fa5d1d2d128ea35b0fde20bf70cd4
Adam Howes <adamhowes@outlook.com>
2021-08-21 14:37:19 +0100

    Data preparation status #029

--- utils/run_data_prep.R
+++ utils/run_data_prep.R
@@ -1,6 +1,9 @@
 #' The 13 GF AGYW countries minus BWA
 iso3 <- c("CMR", "KEN", "LSO", "MOZ", "MWI", "NAM", "SWZ", "TZA", "UGA", "ZAF", "ZMB", "ZWE")
 
+# Error for CMR, MOZ, MWI, NAM, SWZ, ZMB
+# Completed for KEN, LSO, UGA, ZAF, ZWE
+
 #' The names of the reports to run
 reports <- paste0(tolower(iso3), "_data_survey_behav")
 
@@ -26,9 +29,6 @@ sapply(
   }
 )
 
-#' The names of the reports to run
-reports <- paste0(tolower(iso3), "_data_survey_behav")
-
 #' Running without error catching
 sapply(
   reports,
@@ -38,18 +38,6 @@ sapply(
     orderly::orderly_push_archive(report)
   }
 )
-
-#' Running an individual report
-#' * UGA
-#' * TZA
-#' * LSO
-#' * KEN
-
-iso3 <- "KEN"
-report <- paste0(tolower(iso3), "_data_survey_behav")
-id <- orderly::orderly_run(report)
-orderly::orderly_commit(id)
-orderly::orderly_push_archive(report)
 
 #' Running BWA
 
