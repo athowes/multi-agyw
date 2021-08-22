54c37721bbd163f831d1337de14654297f5aa7f5
Adam Howes <adamhowes@outlook.com>
2021-08-22 20:16:26 +0100

    Useful to have this to run after fitting new models. "process" and "plot" are a bit ambiguous, which could be fixed but isn't that much of an issue

---
+++ utils/post_process.R
@@ -0,0 +1,14 @@
+id <- orderly::orderly_run("process_multinomial-smoothed-district-sexbehav")
+orderly::orderly_commit(id)
+
+id <- orderly::orderly_run("process_information-criteria")
+orderly::orderly_commit(id)
+
+id <- orderly::orderly_run("process_variance-proportions")
+orderly::orderly_commit(id)
+
+id <- orderly::orderly_run("plot_temporal-variation")
+orderly::orderly_commit(id)
+
+id <- orderly::orderly_run("plot_within-between-country-variation")
+orderly::orderly_commit(id)
