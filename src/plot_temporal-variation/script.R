b90a89ca315ac89122afd0fa86553bf72233f031
Adam Howes <adamhowes@outlook.com>
2021-08-26 11:50:30 +0100

    Adding the national averages to temporal variation plots now that they are available. Also creating an alternative plot with ribbons for uncertainty #018

--- src/plot_temporal-variation/script.R
+++ src/plot_temporal-variation/script.R
@@ -23,12 +23,22 @@ df <- df %>%
         "Nonregular partner(s) or paid\n for sex (past 12 months)" = "sexnonregplus"
       )
   ) %>%
-  filter(model == "Model 9", age_group != "15-24")
+  filter(
+    model == "Model 9",
+    age_group != "15-24"
+  )
+
+df_subnational <- df %>%
+  filter(!(area_id %in% c("CMR", "KEN", "LSO", "MOZ", "MWI", "NAM", "SWZ", "TZA", "UGA", "ZAF", "ZMB", "ZWE")))
+
+df_national <- setdiff(df, df_subnational)
 
 pdf("temporal-variation.pdf", h = 10, w = 12)
 
-ggplot(df, aes(x = year, y = estimate_smoothed, col = iso3, group = area_name)) +
-  geom_line(alpha = 0.2) +
+ggplot(df_subnational, aes(x = year, y = estimate_smoothed, col = iso3, group = area_name)) +
+  geom_line(alpha = 0.1) +
+  geom_line(data = df_national, aes(x = year, y = estimate_smoothed, col = iso3, group = area_name),
+            size = 1.5) +
   facet_grid(age_group ~  indicator) +
   labs(x = "Year of survey", y = "Posterior mean proportion by region", col = "Country") +
   guides(col = guide_legend(override.aes = list(alpha = 0.5))) +
@@ -36,6 +46,39 @@ ggplot(df, aes(x = year, y = estimate_smoothed, col = iso3, group = area_name)) 
     plot.title = element_text(face = "bold"),
     legend.position = "bottom",
     legend.key.width = unit(4, "lines"),
+    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)
   )
 
 dev.off()
+
+#' Add columns to df_national for the quantiles of the subnational estimates
+df_national <- df_national %>%
+  left_join(
+    df_subnational %>%
+      group_by(iso3, age_group, indicator) %>%
+      summarise(
+        subnational_q975 = quantile(estimate_smoothed, probs = 0.975),
+        subnational_q750 = quantile(estimate_smoothed, probs = 0.750),
+        subnational_q250 = quantile(estimate_smoothed, probs = 0.250),
+        subnational_q025 = quantile(estimate_smoothed, probs = 0.025)
+      ),
+    by = c("iso3", "age_group", "indicator")
+  )
+
+pdf("temporal-variation-alt.pdf", h = 10, w = 12)
+
+ggplot(df_national, aes(x = year, y = estimate_smoothed, fill = iso3, group = area_name)) +
+  geom_line(aes(col = iso3), size = 1.5) +
+  geom_ribbon(aes(ymin = subnational_q025, ymax = subnational_q975), alpha = 0.1) +
+  geom_ribbon(aes(ymin = subnational_q250, ymax = subnational_q750), alpha = 0.3) +
+  facet_grid(age_group ~  indicator) +
+  labs(x = "Year of survey", y = "Posterior mean proportion", col = "Country", fill = "Country") +
+  theme(
+    plot.title = element_text(face = "bold"),
+    legend.position = "bottom",
+    legend.key.width = unit(4, "lines"),
+    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)
+  )
+
+dev.off()
+
