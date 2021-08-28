d4cfa74e15a41bf73ea24fa23a12a176b5117f1d
Adam Howes <adamhowes@outlook.com>
2021-08-28 17:15:41 +0100

    First draft of #034. Not sure how much else there is to do about this

---
+++ src/plot_age-variation/script.R
@@ -0,0 +1,46 @@
+#' Uncomment and run the two line below to resume development of this script
+orderly::orderly_develop_start("plot_age-variation")
+setwd("src/plot_age-variation")
+
+df <- read_csv("depends/every-all-dhs-multinomial-smoothed-district-sexbehav.csv")
+
+df <- df %>%
+  filter(age_group != "Y015_024") %>%
+  mutate(
+    age_group = fct_recode(age_group,
+      "15-19" = "Y015_019",
+      "20-24" = "Y020_024",
+      "25-29" = "Y025_029"
+    ),
+    indicator = fct_recode(indicator,
+      "No sex (past 12 months)" = "nosex12m",
+      "Cohabiting partner" = "sexcohab",
+      "Nonregular partner(s)" = "sexnonregplus"
+    )
+  )
+
+df_age_country <- df %>%
+  group_by(iso3, age_group, indicator) %>%
+  summarise(estimate_smoothed = mean(estimate_smoothed, na.rm = TRUE))
+
+df_age <- df %>%
+  group_by(age_group, indicator) %>%
+  summarise(estimate_smoothed = mean(estimate_smoothed, na.rm = TRUE))
+
+pdf("age-variation.pdf", h = 3.5, w = 6.25)
+
+cbpalette <- c("#56B4E9","#009E73", "#E69F00", "#F0E442","#0072B2","#D55E00","#CC79A7", "#999999")
+
+ggplot(df_age_country, aes(x = age_group, y = estimate_smoothed, group = indicator, col = indicator)) +
+  geom_point(alpha = 0.4) +
+  geom_line(data = df_age, aes(x = age_group, y = estimate_smoothed, group = indicator, col = indicator),
+            size = 1.5, alpha = 0.7) +
+  scale_color_manual(values = cbpalette) +
+  theme_minimal() +
+  scale_y_continuous(labels = function(x) paste0(100 * x, "%")) +
+  labs(x = "Age group", y = "Proportion", col = "Category") +
+  theme(
+    legend.position = "bottom"
+  )
+
+dev.off()
