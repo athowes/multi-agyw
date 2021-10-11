6072c6d8a33cd163e5d406ad8b73023cbaa082de
Adam Howes <adamhowes@outlook.com>
2021-10-11 14:53:45 +0100

    Report to just get the maps for the chosen model (e.g. the final model chosen)

---
+++ src/process_subset-maps/script.R
@@ -0,0 +1,31 @@
+#' Uncomment and run the two line below to resume development of this script
+# orderly::orderly_develop_start("process_subset-maps")
+# setwd("src/process_subset-maps")
+
+#' Want to create .pdf files containing only the results from particular models
+#' (Probably the final model chosen)
+
+iso3 <- c("CMR", "KEN", "LSO", "MOZ", "MWI", "UGA", "ZMB", "ZWE")
+pdfs <- paste0("depends/", tolower(iso3), "_all-dhs-multinomial-smoothed-district-sexbehav.pdf")
+
+if (!dir.exists("tmp")) dir.create("tmp")
+pdfs_output <- paste0("tmp/", tolower(iso3), "_all-dhs-multinomial-smoothed-district-sexbehav.pdf")
+
+#' Get the page numbers of all maps corresponding the chosen model
+pages <- map(pdfs,
+  ~ pdftools::pdf_text(.x) %>%
+    stringr::str_detect(pattern = paste0("\\(Model ", model, "\\)")) %>%
+    which()
+)
+
+#' Create the subsetted .pdf files
+pmap(
+  .l = list(input = pdfs, pages = pages, output = pdfs_output),
+  .f = pdftools::pdf_subset
+)
+
+#' Combine them into one output artefact
+pdftools::pdf_combine(input = pdfs_output, output = paste0("all-dhs-multinomial-smoothed-district-sexbehav.pdf"))
+
+#' Delete the temporary directory
+unlink("tmp", recursive = TRUE)
