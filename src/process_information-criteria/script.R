fcb2690da0650428de0afa7b43d4158fe95cad68
Adam Howes <adamhowes@outlook.com>
2021-12-01 10:36:54 +0000

    Spacing text #25

--- src/process_information-criteria/script.R
+++ src/process_information-criteria/script.R
@@ -2,7 +2,7 @@
 # orderly::orderly_develop_start("process_information-criteria")
 # setwd("src/process_information-criteria")
 
-cbpalette <- c("#56B4E9","#009E73", "#E69F00", "#F0E442","#0072B2","#D55E00","#CC79A7", "#999999")
+cbpalette <- c("#56B4E9","#009E73", "#E69F00", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#999999")
 
 #' aaa_fit_multi-sexbehav-sae
 
