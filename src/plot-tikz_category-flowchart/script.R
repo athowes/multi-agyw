83771bea22915908075c953205f291c367675934
Adam Howes <adamhowes@outlook.com>
2021-12-22 09:59:28 +0000

    The structure for TikZ plot #54

---
+++ src/plot-tikz_category-flowchart/script.R
@@ -0,0 +1,5 @@
+#' Uncomment and run the two line below to resume development of this script
+# orderly::orderly_develop_start("plot-tikz_category-flowchart")
+# setwd("src/plot-tikz_category-flowchart")
+
+system("lualatex category-flowchart.tex")
