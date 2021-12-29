fe900ded1218724b39ddb39d09ebd739132352d0
Adam Howes <adamhowes@outlook.com>
2021-12-29 12:27:04 +0000

    Reduce white-space and consider facet by indicator (commented out for now)

--- src/plot_age-variation/script.R
+++ src/plot_age-variation/script.R
@@ -36,9 +36,15 @@ cbpalette <- c("#56B4E9","#009E73", "#E69F00", "#F0E442", "#0072B2", "#D55E00", 
 ggplot(df_age_country, aes(y = age_group, x = estimate_smoothed, fill = indicator)) +
   ggridges::geom_density_ridges(alpha = 0.7, col = NA) +
   scale_fill_manual(values = cbpalette) +
+  # facet_wrap(~indicator) +
   theme_minimal() +
   scale_x_continuous(labels = function(x) paste0(100 * x, "%")) +
+  scale_y_discrete(expand = expansion(mult = c(0.05, 0))) +
   xlim(0, 1) +
   labs(y = "Age group", x = "Proportion", fill = "Category")
+  # theme(
+  #   legend.position = "bottom",
+  #   legend.key.width = unit(1, "lines")
+  # )
 
 dev.off()
