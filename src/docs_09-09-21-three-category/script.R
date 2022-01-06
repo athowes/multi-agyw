2eb04a7d950ad716683f0661eceda135f7141dfb
Adam Howes <adamhowes@outlook.com>
2022-01-06 16:34:08 +0000

    Port three-category and coverage presentations over to being orderly reports too

---
+++ src/docs_09-09-21-three-category/script.R
@@ -0,0 +1,4 @@
+# orderly::orderly_develop_start("docs_09-09-21-three-category")
+# setwd("src/docs_09-09-21-three-category")
+
+rmarkdown::render("09-09-21-three-category.Rmd")
---
+++ src/docs_30-11-21-coverage/script.R
@@ -0,0 +1,4 @@
+# orderly::orderly_develop_start("docs_30-11-21-coverage")
+# setwd("src/docs_30-11-21-coverage")
+
+rmarkdown::render("30-11-21-coverage.Rmd")
