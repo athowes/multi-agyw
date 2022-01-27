1f2e13fcff8aedb5e18dbc1e1d163c19476cbcc5
Adam Howes <adamhowes@outlook.com>
2022-01-27 11:46:58 +0000

    Second version of this plot. A few more things to add (text, less whitespace, better alignment, thinking more about which labels needed)

--- src/plot_aids-abstract/script.R
+++ src/plot_aids-abstract/script.R
@@ -4,23 +4,35 @@
 
 df <- read_csv("depends/best-3-multinomial-smoothed-district-sexbehav.csv")
 
-#' The following section is copied from plot_within-between-country-variation
+#' The following section is copied mostly from plot_within-between-country-variation
+
+#' To add region column defined based on the UN geoscheme for Africa
+#' https://en.wikipedia.org/wiki/United_Nations_geoscheme_for_Africa
+region_key <- c(
+  "Botswana" = "South",
+  "Cameroon" = "Middle",
+  "Kenya" = "East",
+  "Lesotho" = "South",
+  "Mozambique" = "East",
+  "Malawi" = "East",
+  "Namibia" = "South",
+  "Eswatini" = "South",
+  "Tanzania" = "East",
+  "Uganda" = "East",
+  "South Africa" = "South",
+  "Zambia" = "East",
+  "Zimbabwe" = "East"
+) %>%
+  as.data.frame() %>%
+  rename("region" = ".") %>%
+  tibble::rownames_to_column("iso3")
 
 df <- df %>%
-  filter(
-    age_group != "Y015_024"
-  ) %>%
+  filter(age_group %in% c("Y020_024", "Y025_029")) %>%
   mutate(
     #' Assuming the survey_id is structured as ISO2000DHS
     year = substr(survey_id, 4, 7),
     #' Labels for plot
-    age_group = fct_relevel(age_group, "Y015_024", after = 3) %>%
-      fct_recode(
-        "15-19" = "Y015_019",
-        "20-24" = "Y020_024",
-        "25-29" = "Y025_029",
-        "15-24" = "Y015_024"
-      ),
     indicator =
       fct_recode(indicator,
                  "No sex (past 12 months)" = "nosex12m",
@@ -47,16 +59,19 @@ df <- df %>%
   #' Only the most recent survey in each year
   group_by(iso3) %>%
   filter(year == max(year)) %>%
-  ungroup()
+  ungroup() %>%
+  #' Aggregate up to 20-29
+  group_by(iso3, area_id, indicator) %>%
+  summarise(estimate_smoothed = sum(estimate_smoothed * population_mean) / sum(population_mean)) %>%
+  mutate(age_group = "20-29") %>%
+  ungroup() %>%
+  #' Add region column
+  left_join(region_key, by = "iso3")
+
+priority_iso3 <- c("BWA", "CMR", "KEN", "LSO", "MOZ", "MWI", "NAM", "SWZ", "TZA", "UGA", "ZAF", "ZMB", "ZWE")
 
 df_subnational <- df %>%
-  filter(
-    !(area_id %in% c(
-      "BWA", "CMR", "KEN", "LSO", "MOZ", "MWI", "NAM",
-      "SWZ", "TZA", "UGA", "ZAF", "ZMB", "ZWE"
-    )
-    )
-  )
+  filter(!(area_id %in% c(priority_iso3)))
 
 df_national <- setdiff(df, df_subnational)
 
@@ -72,41 +87,13 @@ df_national <- left_join(
   df_national,
   df_subnational %>%
     filter(iso3 %in% missing_national) %>%
-    group_by(iso3, age_group, indicator) %>%
-    summarise(estimate_smoothed = mean(estimate_smoothed)),
-  by = c("iso3", "age_group", "indicator")
+    group_by(iso3, indicator) %>%
+    summarise(estimate_smoothed = mean(estimate_smoothed, na.rm = TRUE)),
+  by = c("iso3", "indicator")
 ) %>%
   within(., estimate_smoothed.x <- ifelse(!is.na(estimate_smoothed.y), estimate_smoothed.y, estimate_smoothed.x)) %>%
   select(-estimate_smoothed.y) %>%
   rename(estimate_smoothed = estimate_smoothed.x)
-
-#' Add region column
-#' Defined based on the UN geoscheme for Africa
-#' https://en.wikipedia.org/wiki/United_Nations_geoscheme_for_Africa
-region_key <- c(
-  "Botswana" = "South",
-  "Cameroon" = "Middle",
-  "Kenya" = "East",
-  "Lesotho" = "South",
-  "Mozambique" = "East",
-  "Malawi" = "East",
-  "Namibia" = "South",
-  "Eswatini" = "South",
-  "Tanzania" = "East",
-  "Uganda" = "East",
-  "South Africa" = "South",
-  "Zambia" = "East",
-  "Zimbabwe" = "East"
-) %>%
-  as.data.frame() %>%
-  rename("region" = ".") %>%
-  tibble::rownames_to_column("iso3")
-
-df_subnational <- df_subnational %>%
-  left_join(region_key, by = "iso3")
-
-df_national <- df_national %>%
-  left_join(region_key, by = "iso3")
 
 df_national_sort <- df_national %>%
   select(iso3, region) %>%
@@ -129,7 +116,6 @@ cbpalette <- c("#56B4E9","#009E73", "#E69F00", "#F0E442", "#0072B2", "#D55E00", 
 
 plotA <- df_subnational %>%
   filter(
-    age_group %in% c("20-24", "25-29"),
     indicator %in% c("Cohabiting partner", "Nonregular partner(s)")
   ) %>%
   mutate(
@@ -140,7 +126,6 @@ plotA <- df_subnational %>%
   geom_point(
     data = df_national %>%
       filter(
-        age_group %in% c("20-24", "25-29"),
         indicator %in% c("Cohabiting partner", "Nonregular partner(s)")
       ),
     aes(x = fct_rev(iso3), y = estimate_smoothed),
@@ -159,7 +144,7 @@ plotA <- df_subnational %>%
   theme_minimal() +
   theme(
     panel.spacing = unit(1.5, "lines"),
-    legend.position = "bottom",
+    legend.position = "top",
     strip.text = element_text(face = "bold")
   )
 
@@ -168,19 +153,12 @@ plotA <- df_subnational %>%
 areas <- readRDS("depends/areas.rds")
 national_areas <- readRDS("depends/national_areas.rds")
 
-df <- df %>%
+df_subnational <- df_subnational %>%
   left_join(
     select(areas, area_id, geometry),
     by = "area_id"
   ) %>%
   st_as_sf()
-
-priority_iso3 <- c("BWA", "CMR", "KEN", "LSO", "MOZ", "MWI", "NAM", "SWZ", "TZA", "UGA", "ZAF", "ZMB", "ZWE")
-
-df_subnational <- df %>%
-  filter(!(area_id %in% priority_iso3))
-
-df_national <- setdiff(df, df_subnational)
 
 #' Using these just to show missing data for the countries we don't consider in the analysis
 national_areas <- national_areas %>%
@@ -195,8 +173,8 @@ national_areas <- national_areas %>%
   select(-GID_0)
 
 df_national_areas <- crossing(
-  indicator = as.factor(c("No sex (past 12 months)" , "Cohabiting partner", "Nonregular partner(s)")),
-  age_group = c("15-19", "20-24", "25-29"),
+  indicator = as.factor(c("Cohabiting partner", "Nonregular partner(s)")),
+  age_group = c("20-29"),
   iso3 = unique(national_areas$iso3)
 ) %>%
   left_join(national_areas, by = "iso3")
@@ -207,15 +185,12 @@ df_subnational <- bind_rows(
 )
 
 plotB <- df_subnational %>%
-  filter(
-    age_group %in% c("20-24", "25-29"),
-    indicator %in% c("Cohabiting partner", "Nonregular partner(s)")
-  ) %>%
+  filter(indicator %in% c("Cohabiting partner", "Nonregular partner(s)")) %>%
   ggplot(aes(fill = estimate_smoothed)) +
     geom_sf(size = 0.1) +
     scale_fill_viridis_c(option = "C", label = label_percent(), na.value = "#E6E6E6") +
     facet_grid(age_group ~ indicator) +
-    labs(fill = "Estimated proportion") +
+    labs(fill = "") +
     theme_minimal() +
     theme(
       axis.text = element_blank(),
@@ -223,12 +198,11 @@ plotB <- df_subnational %>%
       panel.grid = element_blank(),
       strip.text = element_text(face = "bold"),
       plot.title = element_text(face = "bold"),
-      legend.position = "bottom",
       legend.key.width = unit(2, "lines")
     )
 
-pdf("aids-abstract.pdf", h = 7, w = 8.5)
+pdf("aids-abstract.pdf", h = 7, w = 6.25)
 
-cowplot::plot_grid(plotA, plotB, ncol = 2)
+cowplot::plot_grid(plotA, plotB, ncol = 1)
 
 dev.off()
