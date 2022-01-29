66e7fa691abdfbe625ddeaf6f573e01bebe9d9d2
Adam Howes <adamhowes@outlook.com>
2022-01-29 12:23:29 +0000

    The two factor targetting options. Need to add the one factor. Add to graph. Might be a bit diffiuclt to get all this data into one df. #65

--- src/analyze_infections-averted/script.R
+++ src/analyze_infections-averted/script.R
@@ -7,7 +7,7 @@ df <- read_csv("depends/incidence-district-sexbehav.csv")
 #' Assume some value for how much a hypothetical intervention reduces incidence by (multiplicatively)
 p <- 0.5
 
-inc <- df %>%
+df_area_age_behav <- df %>%
   select(iso3, area_id, age_group, starts_with("population_sex"), starts_with("incidence_sex")) %>%
   pivot_longer(
     cols = starts_with("population") | starts_with("incidence"),
@@ -18,23 +18,54 @@ inc <- df %>%
     id_cols = c("area_id", "age_group", "indicator", "category"),
     names_from = "indicator",
     values_from = "value"
-  ) %>%
-  mutate(infections_averted = incidence * p * population) %>%
-  #' Best opportunities have the highest incidence, take those first
-  arrange(desc(incidence)) %>%
-  mutate(
-    population_cumulative = cumsum(population),
-    infections_averted_cumulative = cumsum(infections_averted)
   )
+
+df_area_age <- df_area_age_behav %>%
+  group_by(area_id, age_group) %>%
+  summarise(
+    incidence = sum(incidence * population, na.rm = TRUE) / sum(population, na.rm = TRUE),
+    population = sum(population, na.rm = TRUE)
+  )
+
+df_area_behav <- df_area_age_behav %>%
+  group_by(area_id, category) %>%
+  summarise(
+    incidence = sum(incidence * population, na.rm = TRUE) / sum(population, na.rm = TRUE),
+    population = sum(population, na.rm = TRUE)
+  )
+
+df_age_behav <- df_area_age_behav %>%
+  group_by(age_group, category) %>%
+  summarise(
+    incidence = sum(incidence * population, na.rm = TRUE) / sum(population, na.rm = TRUE),
+    population = sum(population, na.rm = TRUE)
+  )
+
+compute_infections_averted <- function(df, stratification_name) {
+  df %>%
+    mutate(infections_averted = incidence * p * population) %>%
+    #' Best opportunities have the highest incidence, take those first
+    arrange(desc(incidence)) %>%
+    mutate(
+      population_cumulative = cumsum(population),
+      infections_averted_cumulative = cumsum(infections_averted),
+      stratification = stratification_name
+    )
+}
+
+inc_area_age_behav <- compute_infections_averted(df_area_age_behav, stratification_name = "area_age_behav")
+inc_area_age <- compute_infections_averted(df_area_age, stratification_name = "area_age")
+inc_area_behav <- compute_infections_averted(df_area_behav, stratification_name = "area_behav")
+inc_age_behav <- compute_infections_averted(df_age_behav, stratification_name = "age_behav")
 
 pdf("infections-averted.pdf", h = 4, w = 6.25)
 
-ggplot(inc, aes(x = population_cumulative, y = infections_averted_cumulative)) +
+ggplot(inc_area_age_behav, aes(x = population_cumulative, y = infections_averted_cumulative)) +
   geom_line() +
   theme_minimal() +
   labs(x = "Total population reached",
        y = "Total infections averted",
        title = "Targetting based on age, district, and behaviour",
-       subtitle = "Assume cost to reach each individual is equal, and incidence is reduced\n by 50% by the intervention")
+       subtitle = "Assume cost to reach each individual is equal, and incidence is reduced\nby 50% by the intervention")
 
 dev.off()
