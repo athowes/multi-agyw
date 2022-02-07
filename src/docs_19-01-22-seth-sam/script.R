49e0865d7dda33b0ec879e06ada225a0af52bf49
Adam Howes <adamhowes@outlook.com>
2022-02-07 12:37:04 +0000

    Rename presentation

--- src/docs_19-01-22-seth-sam/script.R
+++
@@ -1,4 +0,0 @@
-# orderly::orderly_develop_start("docs_19-01-22-seth-sam")
-# setwd("src/docs_19-01-22-seth-sam")
-
-rmarkdown::render("19-01-22-seth-sam.Rmd")
---
+++ src/docs_19-01-22-stats-epi-group/script.R
@@ -0,0 +1,4 @@
+# orderly::orderly_develop_start("docs_19-01-22-stats-epi-group")
+# setwd("src/docs_19-01-22-stats-epi-group")
+
+rmarkdown::render("19-01-22-stats-epi-group.Rmd")
