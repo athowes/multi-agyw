9d475e4cdbbcdabc6c3f9918b78da5e6f1b7ca17
Adam Howes <adamhowes@outlook.com>
2022-02-07 19:16:50 +0000

    Systematic way to remove idx columns

--- src/aaa_fit_4-multi-sexbehav-sae/script.R
+++ src/aaa_fit_4-multi-sexbehav-sae/script.R
@@ -277,7 +277,7 @@ dev.off()
 #' Artefact: Smoothed district indicator estimates for multinomial models
 res_df <- res_df %>%
   #' Remove superfluous INLA indicator columns
-  select(-area_idx, -cat_idx, -age_idx, -obs_idx, -age_cat_idx, -area_cat_idx) %>%
+  select(-ends_with("idx"), -ends_with("idx_copy")) %>%
   #' Make it clear which of the estimates are raw and which are from the model (smoothed)
   rename(
     estimate_raw = estimate,
