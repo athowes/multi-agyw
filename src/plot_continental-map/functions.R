9e242be6de5481b4d9751dc251687cd5d7ef331c
Adam Howes <adamhowes@outlook.com>
2022-02-24 11:01:44 +0000

    Continental maps for 3p1 estimates (generalised this script to use a function) #78

---
+++ src/plot_continental-map/functions.R
@@ -0,0 +1,60 @@
+continental_map <- function(df, areas, national_areas) {
+
+  df <- df %>%
+    filter(age_group != "15-24") %>%
+    #' Assuming the survey_id is structured as ISO2000DHS
+    mutate(year = substr(survey_id, 4, 7)) %>%
+    #' Only the most recent survey in each year
+    group_by(iso3) %>%
+    filter(year == max(year)) %>%
+    ungroup() %>%
+    left_join(
+      select(areas, area_id, geometry),
+      by = "area_id"
+    ) %>%
+    st_as_sf()
+
+  priority_iso3 <- multi.utils::priority_iso3()
+
+  df_subnational <- filter(df, !(area_id %in% priority_iso3))
+  df_national <- setdiff(df, df_subnational)
+
+  #' Using these just to show missing data for the countries we don't consider in the analysis
+  national_areas <- national_areas %>%
+    #' These are just chosen manually by looking at countries between CMR and the rest on a map
+    filter(GID_0 %in% c("AGO", "DRC", "CAF", "COD", "COG", "GAB", "GNQ", "RWA", "BDI")
+    ) %>%
+    #' Weird but OK
+    rename(iso3 = NAME_0) %>%
+    select(-GID_0)
+
+  df_national_areas <- crossing(
+    indicator = unique(df_national$indicator),
+    age_group = unique(df_national$age_group),
+    iso3 = unique(national_areas$iso3)
+  ) %>%
+    left_join(national_areas, by = "iso3")
+
+  df_subnational <- bind_rows(
+    df_subnational,
+    df_national_areas
+  )
+
+  ggplot(df_subnational, aes(fill = estimate_smoothed)) +
+    geom_sf(size = 0.1, colour = scales::alpha("grey", 0.25)) +
+    scale_fill_viridis_c(option = "C", label = label_percent(), na.value = "#E6E6E6") +
+    facet_grid(age_group ~ indicator) +
+    labs(fill = "Estimated proportion") +
+    theme_minimal() +
+    theme(
+      axis.text = element_blank(),
+      axis.ticks = element_blank(),
+      panel.grid = element_blank(),
+      strip.text = element_text(face = "bold"),
+      plot.title = element_text(face = "bold"),
+      legend.position = "bottom",
+      legend.key.width = unit(4, "lines"),
+      legend.title = element_text(size = 9),
+      legend.text = element_text(size = 9)
+    )
+}
--- src/plot_continental-map/script.R
+++ src/plot_continental-map/script.R
@@ -2,68 +2,21 @@
 # orderly::orderly_develop_start("plot_continental-map")
 # setwd("src/plot_continental-map")
 
-df <- read_csv("depends/human-best-3-multinomial-smoothed-district-sexbehav.csv")
+df_3 <- read_csv("depends/human-best-3-multinomial-smoothed-district-sexbehav.csv")
+df_3p1 <- read_csv("depends/human-best-3p1-multinomial-smoothed-district-sexbehav.csv")
 areas <- readRDS("depends/areas.rds")
 national_areas <- readRDS("depends/national_areas.rds")
 
-df <- df %>%
-  filter(age_group != "15-24") %>%
-  #' Assuming the survey_id is structured as ISO2000DHS
-  mutate(year = substr(survey_id, 4, 7)) %>%
-  #' Only the most recent survey in each year
-  group_by(iso3) %>%
-  filter(year == max(year)) %>%
-  ungroup() %>%
-  left_join(
-    select(areas, area_id, geometry),
-    by = "area_id"
-  ) %>%
-  st_as_sf()
+pdf("3-continental-map.pdf", h = 8, w = 6.25)
 
-priority_iso3 <- multi.utils::priority_iso3()
+continental_map(df_3, areas, national_areas)
 
-df_subnational <- filter(df, !(area_id %in% priority_iso3))
-df_national <- setdiff(df, df_subnational)
+dev.off()
 
-#' Using these just to show missing data for the countries we don't consider in the analysis
-national_areas <- national_areas %>%
-  #' These are just chosen manually by looking at countries between CMR and the rest on a map
-  filter(GID_0 %in% c("AGO", "DRC", "CAF", "COD", "COG", "GAB", "GNQ", "RWA", "BDI")
-  ) %>%
-  #' Weird but OK
-  rename(iso3 = NAME_0) %>%
-  select(-GID_0)
+pdf("3p1-continental-map.pdf", h = 8, w = 6.75)
 
-df_national_areas <- crossing(
-  indicator = unique(df_national$indicator),
-  age_group = unique(df_national$age_group),
-  iso3 = unique(national_areas$iso3)
-) %>%
-  left_join(national_areas, by = "iso3")
-
-df_subnational <- bind_rows(
-  df_subnational,
-  df_national_areas
-)
-
-pdf("continental-map.pdf", h = 8, w = 6.25)
-
-ggplot(df_subnational, aes(fill = estimate_smoothed)) +
-  geom_sf(size = 0.1, colour = scales::alpha("grey", 0.25)) +
-  scale_fill_viridis_c(option = "C", label = label_percent(), na.value = "#E6E6E6") +
-  facet_grid(age_group ~ indicator) +
-  labs(fill = "Estimated proportion") +
-  theme_minimal() +
-  theme(
-    axis.text = element_blank(),
-    axis.ticks = element_blank(),
-    panel.grid = element_blank(),
-    strip.text = element_text(face = "bold"),
-    plot.title = element_text(face = "bold"),
-    legend.position = "bottom",
-    legend.key.width = unit(4, "lines"),
-    legend.title = element_text(size = 9),
-    legend.text = element_text(size = 9)
-  )
+df_3p1 %>%
+  mutate(iso3 = substr(survey_id, 1, 3)) %>%
+  continental_map(areas, national_areas)
 
 dev.off()
