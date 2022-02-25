245e9dc8f17513053f09e7144d2e44a571be60a7
Adam Howes <adamhowes@outlook.com>
2022-02-25 17:14:16 +0000

    Need this too #78

---
+++ src/plot_temporal-variation/functions.R
@@ -0,0 +1,34 @@
+plot_temporal_interpolation <- function(df) {
+  df %>%
+    filter(
+      !(area_id %in% multi.utils::priority_iso3()),
+      age_group != "15-24"
+    ) %>%
+    pivot_longer(
+      cols = c(starts_with("estimate")),
+      names_to = c(".value", "source"),
+      names_pattern = "(.*)\\_(.*)"
+    ) %>%
+    group_by(survey_id, iso3, indicator, age_group, source) %>%
+    summarise(
+      estimate = mean(estimate, na.rm = TRUE)
+    ) %>%
+    #' Assuming the survey_id is structured as ISO2000DHS
+    mutate(
+      year = as.integer(substr(survey_id, 4, 7)),
+      source = fct_recode(source, "Raw" = "raw", "Smoothed" = "smoothed")
+    ) %>%
+    ggplot(aes(x = year, y = estimate, col = source)) +
+    geom_point(alpha = 0.5) +
+    facet_grid(age_group ~ indicator) +
+    labs(x = "Year of survey", y = "Estimate", col = "Country") +
+    lims(x = c(2000L, 2022L)) +
+    scale_color_manual(values = multi.utils::cbpalette()) +
+    theme_minimal() +
+    theme(
+      plot.title = element_text(face = "bold"),
+      legend.position = "bottom",
+      legend.key.width = unit(4, "lines"),
+      axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)
+    )
+}
