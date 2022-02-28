5f10ebf73099a4763bdf1bf40babba7d3279b4d2
Adam Howes <adamhowes@outlook.com>
2022-02-28 16:50:07 +0000

    Update make files for new fitting scripts

--- make/_make.R
+++ make/_make.R
@@ -7,14 +7,11 @@ src("make/pull_oli_population.R")
 #' 3. Run each of the _data_survey_behav scripts to prepare the input data for modelling
 src("make/run_data_prep.R")
 
-#' 4. Run the four category models for applicable countries
-src("make/run_aaa_fit_4-multinomial-smoothed-district-sexbehav.R")
+#' 4. Run the separate country models for applicable countries
+src("make/run_aaa_fit_multi-sexbehav-sae.R")
 
-#' 5. Run the three category models for all countries
-src("make/run_aaa_fit_3-multinomial-smoothed-district-sexbehav.R")
-
-#' TODO: Run the three category models for all countries jointly
-# src("make/run_fit_3-multisexbehav-sae.R")
+#' 5. TODO: Run the three category models for all countries jointly
+# src("make/run_fit_multisexbehav-sae.R")
 
 #' 6. Post processing of the modelling results
 src("make/post_process.R")
---
+++ make/run_aaa_fit_multi-sexbehav-sae.R
@@ -0,0 +1,41 @@
+#' Run aaa_fit_multi-sexbehav-sae in bulk
+
+report <- "aaa_fit_multi-sexbehav-sae"
+
+#' Three categories, for all 13 countries
+priority_iso3 <- multi.utils::priority_iso3()
+
+#' Lightweight, without PHIA, without interactions
+params <- data.frame(iso3 = priority_iso3, lightweight = TRUE)
+ids <- orderly::orderly_batch(report, params)
+
+#' Get the most recent archived or drafted version of each of the reports
+#' Could also use ids from orderly_batch here: if you've just run it then they should be the same
+recent_ids <- sapply(priority_iso3, function(iso3) {
+    orderly::orderly_search(query = paste0("latest(parameter:iso3 == '", iso3, "')"), report, draft = TRUE)
+})
+
+#' Commit them and push to remote
+sapply(na.omit(recent_ids), function(id) orderly::orderly_commit(id))
+sapply(na.omit(recent_ids), function(id) orderly::orderly_push_archive(name = report, id = id))
+
+report <- "aaa_fit_4-multi-sexbehav-sae"
+
+#' Four categories, for only those countries which have any surveys with a dedicated sex paid question
+recent <- orderly::orderly_latest(name = "plot_available-surveys")
+available_surveys <- read_csv(paste0("archive/plot_available-surveys/", recent, "/available-surveys.csv"))
+
+giftsvar_iso3 <- available_surveys %>%
+  filter(giftsvar == 1) %>%
+  pull(iso3) %>%
+  unique()
+
+params <- data.frame(iso3 = giftsvar_iso3, lightweight = TRUE)
+ids <- orderly::orderly_batch(report, params)
+
+recent_ids <- sapply(priority_iso3, function(iso3) {
+  orderly::orderly_search(query = paste0("latest(parameter:iso3 == '", iso3, "')"), report, draft = TRUE)
+})
+
+sapply(na.omit(recent_ids), function(id) orderly::orderly_commit(id))
+sapply(na.omit(recent_ids), function(id) orderly::orderly_push_archive(name = report, id = id))
