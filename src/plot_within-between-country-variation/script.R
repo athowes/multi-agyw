1e184305a6c3d608598dba26148d4b46ede6f458
Adam Howes <adamhowes@outlook.com>
2022-03-02 15:02:05 +0000

    Look at FSW estimates adjusted versus unadjusted

--- src/plot_within-between-country-variation/script.R
+++ src/plot_within-between-country-variation/script.R
@@ -2,93 +2,17 @@
 # orderly::orderly_develop_start("plot_within-between-country-variation")
 # setwd("src/plot_within-between-country-variation")
 
-df <- read_csv("depends/human-best-3p1-multi-sexbehav-sae.csv")
+df_adjusted <- read_csv("depends/adjust-best-3p1-multi-sexbehav-sae.csv") %>%
+  update_naming()
 
-df <- df %>%
-  filter(age_group != "15-24") %>%
-  mutate(
-    #' Assuming the survey_id is structured as ISO2000DHS
-    year = substr(survey_id, 4, 7),
-  ) %>%
-  #' Only the most recent survey in each year
-  group_by(iso3) %>%
-  filter(year == max(year)) %>%
-  ungroup()
-
-df_subnational <- df %>%
-  filter(!(area_id %in% multi.utils::priority_iso3()))
-
-df_national <- setdiff(df, df_subnational)
-
-#' Countries that are missing a national level aggregate
-#' This is a temporary solution and we should go back over and get these aggregates in properly
-missing_national <- df_national %>%
-  filter(is.na(estimate_smoothed)) %>%
-  pull(iso3) %>%
-  unique()
-
-#' Overwriting NAs left_join (there must be a better way to do this, looks a lot simpler in data.table)
-df_national <- left_join(
-  df_national,
-  df_subnational %>%
-    filter(iso3 %in% missing_national) %>%
-    group_by(iso3, age_group, indicator) %>%
-    summarise(estimate_smoothed = mean(estimate_smoothed)),
-  by = c("iso3", "age_group", "indicator")
-  ) %>%
-  within(., estimate_smoothed.x <- ifelse(!is.na(estimate_smoothed.y), estimate_smoothed.y, estimate_smoothed.x)) %>%
-  select(-estimate_smoothed.y) %>%
-  rename(estimate_smoothed = estimate_smoothed.x)
-
-#' Add region column
-#' Defined based on the UN geoscheme for Africa
-#' https://en.wikipedia.org/wiki/United_Nations_geoscheme_for_Africa
-region_key <- c(
-  "Botswana" = "South",
-  "Cameroon" = "Middle",
-  "Kenya" = "East",
-  "Lesotho" = "South",
-  "Mozambique" = "East",
-  "Malawi" = "East",
-  "Namibia" = "South",
-  "Eswatini" = "South",
-  "Tanzania" = "East",
-  "Uganda" = "East",
-  "South Africa" = "South",
-  "Zambia" = "East",
-  "Zimbabwe" = "East"
-) %>%
-  as.data.frame() %>%
-  rename("region" = ".") %>%
-  tibble::rownames_to_column("iso3")
-
-df_subnational <- df_subnational %>%
-  left_join(region_key, by = "iso3")
-
-df_national <- df_national %>%
-  left_join(region_key, by = "iso3")
-
-df_national_sort <- df_national %>%
-  select(iso3, region) %>%
-  unique() %>%
-  group_by(region) %>%
-  arrange(iso3, .by_group = TRUE) %>%
-  ungroup() %>%
-  mutate(iso3_sort_order = row_number()) %>%
-  select(iso3, iso3_sort_order)
-
-df_subnational <- df_subnational %>%
-  left_join(
-    df_national_sort,
-    by = "iso3"
-  )
+adjusted <- prepare_estimates(df_adjusted)
+df_subnational <- adjusted$df_subnational
+df_national <- adjusted$df_national
 
 pdf("within-between-country-variation.pdf", h = 7, w = 8)
 
 df_subnational %>%
-  mutate(
-    iso3 = reorder(iso3, iso3_sort_order)
-  ) %>%
+  mutate(iso3 = reorder(iso3, iso3_sort_order)) %>%
   ggplot(aes(x = fct_rev(iso3), y = estimate_smoothed, col = region)) +
     geom_jitter(width = 0.1, alpha = 0.6, shape = 20) +
     geom_point(data = df_national, aes(x = fct_rev(iso3), y = estimate_smoothed),
@@ -105,6 +29,45 @@ df_subnational %>%
       legend.position = "bottom",
       strip.text = element_text(face = "bold")
     )
+
+dev.off()
+
+#' Have a closer look at the FSW estimates and check that the Johnston adjustment has worked
+df_unadjusted <- read_csv("depends/best-3p1-multi-sexbehav-sae.csv") %>%
+  update_naming()
+unadjusted <- prepare_estimates(df_unadjusted)
+
+df_subnational <- bind_rows(
+  mutate(df_subnational, adjustment = "Johnston"),
+  mutate(unadjusted$df_subnational, adjustment = "None")
+)
+
+df_national <- bind_rows(
+  mutate(df_national, adjustment = "Johnston"),
+  mutate(unadjusted$df_national, adjustment = "None")
+)
+
+pdf("within-between-country-variation-fsw.pdf", h = 6, w = 6.25)
+
+df_subnational %>%
+  filter(indicator == "YWKP") %>%
+  mutate(iso3 = reorder(iso3, iso3_sort_order)) %>%
+  ggplot(aes(x = fct_rev(iso3), y = estimate_smoothed, col = region)) +
+  geom_jitter(width = 0.1, alpha = 0.6, shape = 20) +
+  geom_point(data = df_national, aes(x = fct_rev(iso3), y = estimate_smoothed),
+             shape = 21, size = 2, fill = "white", col = "black", alpha = 0.9) +
+  facet_grid(adjustment ~ age_group) +
+  scale_color_manual(values = multi.utils::cbpalette()) +
+  scale_y_continuous(labels = function(x) paste0(100 * x, "%")) +
+  coord_flip() +
+  labs(x = "", y = "Proportion", col = "Regions of sub-Saharan Africa") +
+  guides(colour = guide_legend(override.aes = list(alpha = 0.9, size = 5))) +
+  theme_minimal() +
+  theme(
+    panel.spacing = unit(1.5, "lines"),
+    legend.position = "bottom",
+    strip.text = element_text(face = "bold")
+  )
 
 dev.off()
 
