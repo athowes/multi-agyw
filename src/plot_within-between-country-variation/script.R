54e021969833ffebdf70086df5632330c8ad66fa
Adam Howes <adamhowes@outlook.com>
2022-03-03 10:53:54 +0000

    Getting this script to run (don't have population in there yet so later part breaks)

--- src/plot_within-between-country-variation/script.R
+++ src/plot_within-between-country-variation/script.R
@@ -3,7 +3,7 @@
 # setwd("src/plot_within-between-country-variation")
 
 df_adjusted <- read_csv("depends/adjust-best-3p1-multi-sexbehav-sae.csv") %>%
-  update_naming()
+  multi.utils::update_naming()
 
 adjusted <- prepare_estimates(df_adjusted)
 df_subnational <- adjusted$df_subnational
@@ -34,7 +34,7 @@ dev.off()
 
 #' Have a closer look at the FSW estimates and check that the Johnston adjustment has worked
 df_unadjusted <- read_csv("depends/best-3p1-multi-sexbehav-sae.csv") %>%
-  update_naming()
+  multi.utils::update_naming()
 unadjusted <- prepare_estimates(df_unadjusted)
 
 df_subnational <- bind_rows(
@@ -71,65 +71,65 @@ df_subnational %>%
 
 dev.off()
 
-#' Quantification of points discussed
-#' District level quantile information (age disaggregated)
-df_subnational %>%
-  select(iso3, indicator, age_group, estimate_smoothed) %>%
-  group_by(indicator, age_group) %>%
-  summarise(
-    lower = 100 * quantile(estimate_smoothed, probs = 0.025, na.rm = TRUE),
-    median = 100 * median(estimate_smoothed, na.rm = TRUE),
-    upper = 100 * quantile(estimate_smoothed, probs = 0.975, na.rm = TRUE)
-  )
-
-#' District level quantile information aggregated by age (population weighted)
-df_subnational %>%
-  select(iso3, area_id, indicator, age_group, estimate_smoothed, population_mean) %>%
-  group_by(area_id, indicator) %>%
-  summarise(
-    estimate_smoothed = sum(estimate_smoothed * population_mean) / sum(population_mean)
-  ) %>%
-  ungroup() %>%
-  group_by(indicator) %>%
-  summarise(
-    lower = 100 * quantile(estimate_smoothed, probs = 0.025, na.rm = TRUE),
-    median = 100 * median(estimate_smoothed, na.rm = TRUE),
-    upper = 100 * quantile(estimate_smoothed, probs = 0.975, na.rm = TRUE)
-  )
-
-#' What proportion of 15-19 year-olds are cohabiting in MOZ?
-df_national %>%
-  filter(
-    indicator == "Cohabiting partner",
-    age_group == "15-19",
-    iso3 == "Mozambique"
-  ) %>%
-  mutate(estimate_smoothed = 100 * estimate_smoothed) %>%
-  pull(estimate_smoothed) %>%
-  signif(digits = 3)
-
-#' District level quantile information aggregated by age (population weighted)
-#' What proportion of 20-29 year-olds are cohabiting versus with nonregular partner(s),
-#' above and below the south / east dividing border (UN geoscheme)?
-df_subnational %>%
-  mutate(
-    border = ifelse(region %in% c("East", "Middle"), "Above", "Below")
-  ) %>%
-  select(iso3, area_id, indicator, age_group, border, estimate_smoothed, population_mean) %>%
-  filter(
-    indicator %in% c("Cohabiting partner", "Nonregular partner(s)"),
-    age_group %in% c("20-24", "25-29")
-  ) %>%
-  group_by(area_id, indicator) %>%
-  summarise(
-    estimate_smoothed = sum(estimate_smoothed * population_mean) / sum(population_mean),
-    border = max(border) #' Should just be a way to keep border column
-  ) %>%
-  ungroup() %>%
-  group_by(border, indicator) %>%
-  summarise(
-    lower = 100 * quantile(estimate_smoothed, probs = 0.025, na.rm = TRUE),
-    median = 100 * median(estimate_smoothed, na.rm = TRUE),
-    upper = 100 * quantile(estimate_smoothed, probs = 0.975, na.rm = TRUE)
-  ) %>%
-  mutate(across(lower:upper, ~signif(.x, digits = 3)))
+#' #' Quantification of points discussed
+#' #' District level quantile information (age disaggregated)
+#' df_subnational %>%
+#'   select(iso3, indicator, age_group, estimate_smoothed) %>%
+#'   group_by(indicator, age_group) %>%
+#'   summarise(
+#'     lower = 100 * quantile(estimate_smoothed, probs = 0.025, na.rm = TRUE),
+#'     median = 100 * median(estimate_smoothed, na.rm = TRUE),
+#'     upper = 100 * quantile(estimate_smoothed, probs = 0.975, na.rm = TRUE)
+#'   )
+#'
+#' #' District level quantile information aggregated by age (population weighted)
+#' df_subnational %>%
+#'   select(iso3, area_id, indicator, age_group, estimate_smoothed, population_mean) %>%
+#'   group_by(area_id, indicator) %>%
+#'   summarise(
+#'     estimate_smoothed = sum(estimate_smoothed * population_mean) / sum(population_mean)
+#'   ) %>%
+#'   ungroup() %>%
+#'   group_by(indicator) %>%
+#'   summarise(
+#'     lower = 100 * quantile(estimate_smoothed, probs = 0.025, na.rm = TRUE),
+#'     median = 100 * median(estimate_smoothed, na.rm = TRUE),
+#'     upper = 100 * quantile(estimate_smoothed, probs = 0.975, na.rm = TRUE)
+#'   )
+#'
+#' #' What proportion of 15-19 year-olds are cohabiting in MOZ?
+#' df_national %>%
+#'   filter(
+#'     indicator == "Cohabiting partner",
+#'     age_group == "15-19",
+#'     iso3 == "Mozambique"
+#'   ) %>%
+#'   mutate(estimate_smoothed = 100 * estimate_smoothed) %>%
+#'   pull(estimate_smoothed) %>%
+#'   signif(digits = 3)
+#'
+#' #' District level quantile information aggregated by age (population weighted)
+#' #' What proportion of 20-29 year-olds are cohabiting versus with nonregular partner(s),
+#' #' above and below the south / east dividing border (UN geoscheme)?
+#' df_subnational %>%
+#'   mutate(
+#'     border = ifelse(region %in% c("East", "Middle"), "Above", "Below")
+#'   ) %>%
+#'   select(iso3, area_id, indicator, age_group, border, estimate_smoothed, population_mean) %>%
+#'   filter(
+#'     indicator %in% c("Cohabiting partner", "Nonregular partner(s)"),
+#'     age_group %in% c("20-24", "25-29")
+#'   ) %>%
+#'   group_by(area_id, indicator) %>%
+#'   summarise(
+#'     estimate_smoothed = sum(estimate_smoothed * population_mean) / sum(population_mean),
+#'     border = max(border) #' Should just be a way to keep border column
+#'   ) %>%
+#'   ungroup() %>%
+#'   group_by(border, indicator) %>%
+#'   summarise(
+#'     lower = 100 * quantile(estimate_smoothed, probs = 0.025, na.rm = TRUE),
+#'     median = 100 * median(estimate_smoothed, na.rm = TRUE),
+#'     upper = 100 * quantile(estimate_smoothed, probs = 0.975, na.rm = TRUE)
+#'   ) %>%
+#'   mutate(across(lower:upper, ~signif(.x, digits = 3)))
