060be379aef175b70d4a4f9b10bf52b9e964e0ed
Adam Howes <adamhowes@outlook.com>
2022-03-09 14:04:05 +0000

    Add basic temporal interpolation plot with all years #85

--- src/plot_temporal-variation/script.R
+++ src/plot_temporal-variation/script.R
@@ -70,3 +70,43 @@ dev.off()
 #'   )
 #'
 #' dev.off()
+
+df <- read_csv("depends/multi-sexbehav-sae.csv")
+
+pdf("temporal-interpolation.pdf", h = 6, w = 6.25)
+
+df_ribbon <- df %>%
+  mutate(iso3 = substr(area_id, 1, 3)) %>%
+  group_by(indicator, iso3, year, age_group) %>%
+  summarise(
+    mean_smoothed = mean(estimate_smoothed),
+    lower_smoothed = quantile(estimate_smoothed, probs = 0.05),
+    upper_smoothed = quantile(estimate_smoothed, probs = 0.95)
+  )
+
+df_data <- df %>%
+  filter(!is.na(estimate_raw)) %>%
+  group_by(indicator, survey_id, age_group) %>%
+  summarise(mean_raw = mean(estimate_raw)) %>%
+  mutate(
+    iso3 = substr(survey_id, 1, 3),
+    year = as.numeric(substr(survey_id, 4, 7)),
+    type = substr(survey_id, 8, 11)
+  )
+
+df_ribbon %>%
+  split(.$indicator) %>%
+  lapply(function(x)
+    ggplot(x, aes(x = year, y = mean_smoothed)) +
+      geom_ribbon(aes(ymin = lower_smoothed, ymax = upper_smoothed), alpha = 0.5) +
+      geom_line() +
+      geom_point(
+        data = filter(df_data, indicator == x$indicator[1]),
+        aes(x = year, y = mean_raw, col = type)
+      ) +
+      facet_grid(iso3 ~ age_group) +
+      theme_minimal() +
+      labs(title = paste0(x$indicator[1]), x = "Year", y = "Estimate")
+  )
+
+dev.off()
