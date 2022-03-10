32e06bae5de68d707ade1199e514a328e6db0833
Adam Howes <adamhowes@outlook.com>
2022-03-10 11:39:07 +0000

    Function rather than repeat, and move to all possible stratifications

--- src/plot_infections-averted/script.R
+++ src/plot_infections-averted/script.R
@@ -5,6 +5,7 @@
 df <- read_csv("depends/incidence-district-sexbehav.csv")
 
 #' Assume some value for how much a hypothetical intervention reduces incidence by (multiplicatively)
+#' A value of one corresponds to "finding" an infection (or infections averted if the intervention is 100% effective)
 p <- 1
 
 df_area_age_behav <- df %>%
@@ -20,43 +21,38 @@ df_area_age_behav <- df %>%
     values_from = "value"
   )
 
-df_area_age <- df_area_age_behav %>%
-  group_by(area_id, age_group) %>%
-  summarise(
-    incidence = sum(incidence * population, na.rm = TRUE) / sum(population, na.rm = TRUE),
-    population = sum(population, na.rm = TRUE)
-  ) %>%
-  ungroup() %>%
-  mutate(iso3 = substr(area_id, 1, 3))
+#' https://tidyeval.tidyverse.org/dplyr.html
+aggregate_inc_pop <- function(df, ...) {
+  df <- df %>%
+    group_by(...) %>%
+    summarise(
+      incidence = sum(incidence * population, na.rm = TRUE) / sum(population, na.rm = TRUE),
+      population = sum(population, na.rm = TRUE)
+    ) %>%
+    ungroup()
 
-df_area_behav <- df_area_age_behav %>%
-  group_by(area_id, category) %>%
-  summarise(
-    incidence = sum(incidence * population, na.rm = TRUE) / sum(population, na.rm = TRUE),
-    population = sum(population, na.rm = TRUE)
-  ) %>%
-  ungroup() %>%
-  mutate(iso3 = substr(area_id, 1, 3))
+  if("area_id" %in% names(df)) {
+    df <- mutate(df, iso3 = substr(area_id, 1, 3))
+  }
 
-df_age_behav <- df_area_age_behav %>%
-  group_by(age_group, category) %>%
-  summarise(
-    incidence = sum(incidence * population, na.rm = TRUE) / sum(population, na.rm = TRUE),
-    population = sum(population, na.rm = TRUE)
-  ) %>%
-  ungroup()
+  df
+}
 
-df_none <- df_area_age_behav %>%
-  summarise(
-    incidence = sum(incidence * population, na.rm = TRUE) / sum(population, na.rm = TRUE),
-    population = sum(population, na.rm = TRUE)
-  ) %>%
-  ungroup()
+df_area_age <- aggregate_inc_pop(df_area_age_behav, area_id, age_group)
+df_area_behav <- aggregate_inc_pop(df_area_age_behav, area_id, category)
+df_age_behav <- aggregate_inc_pop(df_area_age_behav, age_group, category)
+df_area <- aggregate_inc_pop(df_area_age_behav, area_id)
+df_age <- aggregate_inc_pop(df_area_age_behav, age_group)
+df_behav <-aggregate_inc_pop(df_area_age_behav, category)
+df_none <- aggregate_inc_pop(df_area_age_behav)
 
 inc_area_age_behav <- compute_infections_averted(df_area_age_behav, stratification_name = "Area, age, behaviour")
 inc_area_age <- compute_infections_averted(df_area_age, stratification_name = "Area, age")
 inc_area_behav <- compute_infections_averted(df_area_behav, stratification_name = "Area, behaviour")
 inc_age_behav <- compute_infections_averted(df_age_behav, stratification_name = "Age, behaviour")
+inc_area <- compute_infections_averted(df_area, stratification_name = "Area")
+inc_age <- compute_infections_averted(df_age, stratification_name = "Age")
+inc_behav <- compute_infections_averted(df_behav, stratification_name = "Behaviour")
 inc_none <- compute_infections_averted(df_none, stratification_name = "None")
 
 inc <- bind_rows(
@@ -64,6 +60,9 @@ inc <- bind_rows(
   inc_area_age,
   inc_area_behav,
   inc_age_behav,
+  inc_area,
+  inc_age,
+  inc_behav,
   inc_none
 )
 
@@ -86,7 +85,7 @@ pdf("infections-averted.pdf", h = 4, w = 6.25)
 ggplot(inc, aes(x = population_cumulative, y = infections_averted_cumulative_improvement, col = stratification)) +
   geom_line(alpha = 0.8, size = 0.7) +
   theme_minimal() +
-  scale_color_manual(values = multi.utils::cbpalette()[-c(4, 5)]) +
+  scale_color_manual(values = multi.utils::cbpalette()) +
   scale_y_continuous(labels = label_number(scale = 1e-3)) +
   scale_x_continuous(labels = label_number(scale = 1e-6)) +
   labs(
