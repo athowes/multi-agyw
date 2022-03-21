adffc15bee99c27943791b60fc8ebae8536c2c89
Adam Howes <adamhowes@outlook.com>
2022-03-21 14:42:04 +0000

    Add FSW question respondents by age

--- src/plot_available-surveys/script.R
+++ src/plot_available-surveys/script.R
@@ -173,6 +173,17 @@ df %>%
   pull(Y015_029) %>%
   sum()
 
+#' What about disaggregating these AGYW by age?
+#' TODO: This isn't quite right because some 25-29 are not asked FSW question in DHS surveys
+#' Might fix this manually
+df %>%
+  filter(survey_id %in% giftsvar_surveys) %>%
+  summarise(
+    Y015_019 = sum(Y015_019),
+    Y020_024 = sum(Y020_024),
+    Y025_029 = sum(Y025_029)
+  )
+
 #' What was the median number of surveys by country, and the range?
 (number_of_surveys <- df %>%
   group_by(iso3) %>%
