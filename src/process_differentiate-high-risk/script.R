55c4d27a39fc2a77d9e510ddd557bc9be7908f94
Adam Howes <adamhowes@outlook.com>
2022-03-22 22:49:03 +0000

    Renamed upstream

--- src/process_differentiate-high-risk/script.R
+++ src/process_differentiate-high-risk/script.R
@@ -2,7 +2,7 @@
 # orderly::orderly_develop_start("process_differentiate-high-risk")
 # setwd("src/process_differentiate-high-risk")
 
-df_3_aaa <- read_csv("depends/best-3-multi-sexbehav-sae.csv")
+df_3_aaa <- read_csv("depends/best-3-aaa-multi-sexbehav-sae.csv")
 df_3 <- read_csv("depends/multi-sexbehav-sae.csv") %>%
   filter(model == "Model 4") #' Temporary solution, should be earlier in pipeline
 
