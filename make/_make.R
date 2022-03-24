5fe2471ad4c6438ad4e6f1c9fe096e3b8cfd7fe5
Adam Howes <adamhowes@outlook.com>
2022-03-24 15:02:15 +0000

    Update make folder

--- make/_make.R
+++ make/_make.R
@@ -10,8 +10,14 @@ src("make/run_data_prep.R")
 #' 4. Run the separate country models for applicable countries
 src("make/run_aaa_fit_multi-sexbehav-sae.R")
 
-#' 5. TODO: Run the three category models for all countries jointly
-# src("make/run_fit_multisexbehav-sae.R")
+#' 5. Run the three category models for all countries jointly
+src("make/run_fit_multi-sexbehav-sae.R")
 
-#' 6. Post processing of the modelling results
+#' 6. Differentiate the highest risk cateogry using a logistic regression model
+src("make/high_risk_differentiation.R")
+
+#' 7. Post processing (including e.g. figures) of the modelling results
 src("make/post_process.R")
+
+#' 8. Create documentation
+src("make/docs.R")
