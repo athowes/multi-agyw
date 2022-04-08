f1276740ce53722b152546855fa338d0ea34a804
Adam Howes <adamhowes@outlook.com>
2022-04-08 20:01:05 +0100

    Placeholder for #81

---
+++ src/process_prevalence/script.R
@@ -0,0 +1,5 @@
+#' Uncomment and run the two line below to resume development of this script
+# orderly::orderly_develop_start("process_prevalence")
+# setwd("src/process_prevalence")
+
+analysis_level <- multi.utils::analysis_level()
