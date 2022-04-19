7091f1e7cd7cf33a940726be093ede57d7333e26
Adam Howes <adamhowes@outlook.com>
2022-04-19 22:21:06 +0100

    Try running all eight models #99

--- make/5_run_fit_multi-sexbehav-sae.R
+++ make/5_run_fit_multi-sexbehav-sae.R
@@ -1,10 +1,11 @@
 source("make/utils.R")
 
-#' Data pre-processing step
-run_commit_push("process_all-data", push = FALSE)
-
 #' Run model for all countries
 id <- orderly::orderly_run("fit_multi-sexbehav-sae", parameters = list(lightweight = TRUE, fewer_countries = TRUE))
+orderly::orderly_commit(id)
+
+#' Attempted to run this but R crash on 4x :'(
+id <- orderly::orderly_run("fit_multi-sexbehav-sae", parameters = list(fewer_countries = TRUE))
 orderly::orderly_commit(id)
 
 id <- orderly::orderly_run("fit_multi-sexbehav-sae", parameters = list(lightweight = TRUE))
