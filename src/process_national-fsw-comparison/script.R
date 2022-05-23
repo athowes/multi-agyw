55f7155aef5e0ccd0433aef56252c638ce15414f
athowes <adamhowes@outlook.com>
2022-05-23 17:20:39 +0100

    Transactional adjustment using age-disagg-fsw and comparisons between Oli and Johnston #115

--- src/process_national-fsw-comparison/script.R
+++ src/process_national-fsw-comparison/script.R
@@ -4,20 +4,25 @@
 
 #' All available FSW data sources
 #'
-#' * Paper: "Deriving and interpreting population size estimates for adolescent and young key populations at higher
-#' risk of HIV transmission: men who have sex with men, female sex workers and transgender persons"
+#' * Paper: "Deriving and interpreting population size estimates for adolescent
+#'   and young key populations at higher risk of HIV transmission: men who have
+#'   sex with men, female sex workers and transgender persons"
 #' * First author: Lisa Johnston
 #' * Areas: National
 #' * Age groups: 15-19, 20-24, 25-49
-#' * Code: Could ask Kinh for it
+#' * Code: Similar to process_age-disagg-fsw
 #'
-#' * Paper: "Estimating key population size, HIV prevalence, and ART coverage for sub-Saharan Africa at the national level"
+#' * Paper: "Estimating key population size, HIV prevalence, and ART coverage
+#'   for sub-Saharan Africa at the national level"
 #' * First author: Oliver Stevens
 #' * Areas: National
 #' * Age groups: 15-49
-#' * Code: Could ask Oli for it
+#' * Code: https://github.com/osymandius/kp-data
+#' * Notes: I've age disaggregated these estimates to five year age bands using
+#'   the same method as Johnston et al. in the report process_age-disagg-fsw
 #'
-#' * Paper: "Mapping female sex worker prevalence (aged 15-49 years) in sub-Saharan Africa"
+#' * Paper: "Mapping female sex worker prevalence (aged 15-49 years) in
+#'   sub-Saharan Africa"
 #' * First author: Ian Laga
 #' * Areas: Sub-national
 #' * Age-groups: 15-49
@@ -26,70 +31,46 @@
 analysis_level <- multi.utils::analysis_level()
 priority_iso3 <- multi.utils::priority_iso3()
 
-#' Get age-stratified population total sizes from Naomi model outputs
-sharepoint <- spud::sharepoint$new("https://imperiallondon.sharepoint.com/")
+pop <- read_csv("depends/interpolated_population.csv")
+age_groups <- c("Y015_019", "Y020_024", "Y025_029", "Y030_034", "Y035_039", "Y040_044", "Y045_049")
 
-url <- "sites/HIVInferenceGroup-WP/Shared Documents/Data/Spectrum files/2021 naomi/Naomi datasets in R/naomi3.rds"
-path <- sharepoint$download(URLencode(url))
-naomi3 <- readRDS(path)
-
-url <- "sites/HIVInferenceGroup-WP/Shared Documents/Data/Spectrum files/2021 naomi/areas-extract/naomi-2021-results_pooled-area-hierarchy.csv"
-path <- sharepoint$download(URLencode(url))
-area_hierarchy <- read_csv(path)
-
-naomi3 <- naomi3 %>%
+pop <- pop %>%
+  mutate(
+    iso3 = substr(area_id, 1, 3)
+  ) %>%
   filter(
+    age_group %in% age_groups,
+    sex == "female",
     iso3 %in% priority_iso3,
-    indicator_label == "Population",
-    #' These are the age groups we are considering,
-    age_group_label %in% c("15-19", "20-24", "25-29", "25-29", "15-49"),
-    #' Only female
-    sex == "female"
+    year == 2018,
   ) %>%
-  #' The most recent estimates
-  group_by(iso3) %>%
-  filter(calendar_quarter == max(calendar_quarter)) %>%
-  ungroup() %>%
-  #' Merge with area hierarchy data
-  left_join(
-    select(area_hierarchy, area_id, parent_area_id),
-    by = "area_id"
+  pivot_wider(
+    names_from = age_group,
+    values_from = population
   ) %>%
-  select(iso3, area_id, area_level, age_group = age_group_label, population_mean = mean, parent_area_id)
-
-#' The BWA and CMR population are at one level too low
-naomi3_aggregates <- naomi3 %>%
-  filter(iso3 %in% c("BWA", "CMR")) %>%
-  group_by(parent_area_id) %>%
-  summarise(
-    iso3 = iso3,
-    age_group = age_group,
-    population_mean = sum(population_mean)
+  mutate(
+    Y025_049 = Y025_029 + Y030_034 + Y035_039 + Y040_044 + Y045_049,
+    Y015_049 = Y015_019 + Y020_024 + Y025_049,
+    Y015_029 = Y015_019 + Y020_024 + Y025_029,
   ) %>%
-  rename(area_id = parent_area_id) %>%
-  mutate(area_level = as.numeric(substr(area_id, 5, 5)))
-
-naomi3 <- bind_rows(naomi3, naomi3_aggregates) %>%
-  split(.$iso3) %>%
-  lapply(function(x) {
-    filter(x, area_level == analysis_level[x$iso3[1]])
-  }) %>%
-  bind_rows() %>%
-  select(-parent_area_id)
-
-naomi3_national <- naomi3 %>%
-  group_by(iso3, age_group) %>%
-  summarise(population_mean = sum(population_mean))
-
-naomi3 %>% pull(iso3) %>% unique()
+  pivot_longer(
+    cols = starts_with("Y0"),
+    names_to = "age_group",
+    values_to = "population"
+  ) %>%
+  mutate(
+    age_group = recode_factor(
+      age_group,
+      "Y015_019" = "15-19", "Y020_024" = "20-24", "Y025_029" = "25-29",
+      "Y030_034" = "30-34", "Y035_039" = "35-39", "Y040_044" = "40-44",
+      "Y045_049" = "45-49", "Y025_049" = "25-49", "Y015_049" = "15-49",
+      "Y015_029" = "15-29"
+    )
+  )
 
 #' Read in the population size estimates (PSEs) for AYKP
 johnston <- read_excel("aykp_pse_july17.xlsx", sheet = "FSW", range = "A3:F187")
 names(johnston) <- c("region", "country", "size_15-19", "size_20-24", "size_15-24", "size_25-49")
-
-johnston$region %>% unique()
-
-johnston %>% filter(region %in% c("ESA", "MENA", "WCA"))
 
 #' iso3 codes from https://gist.github.com/tadast/8827699
 country_codes <- read_csv("countries_codes_and_coordinates.csv") %>%
@@ -99,7 +80,12 @@ national_areas <- readRDS("depends/national_areas.rds")
 
 johnston <- johnston %>%
   left_join(country_codes) %>%
-  pivot_longer(cols = contains("size"), names_prefix = "size_", names_to = "age_group", values_to = "est_total_johnston") %>%
+  pivot_longer(
+    cols = contains("size"),
+    names_prefix = "size_",
+    names_to = "age_group",
+    values_to = "total"
+  ) %>%
   filter(
     region %in% c("ESA", "WCA"),
     iso3 %in% priority_iso3,
@@ -108,108 +94,100 @@ johnston <- johnston %>%
   select(-region) %>%
   filter(iso3 %in% priority_iso3) %>%
   left_join(
-    naomi3_national,
+    pop %>%
+      filter(area_id %in% priority_iso3),
     by = c("age_group", "iso3")
   ) %>%
-  mutate(est_proportion_johnston = est_total_johnston / population_mean)
+  mutate(proportion = total / population)
 
 #' Check that all of the priority ISO3 are in there
 stopifnot(unique(johnston$iso3) %>% length() == 13)
 
-pdf("johnston-fsw-data.pdf", h = 8, w = 6.25)
+pdf("johnston-fsw-data.pdf", h = 4, w = 6.25)
 
 johnston_sf <- johnston %>%
+  pivot_longer(
+    cols = c(total, proportion),
+    names_to = "indicator",
+    values_to = "estimate"
+  ) %>%
   left_join(
     select(national_areas, "iso3" = "area_id"),
     by = "iso3"
   ) %>%
   st_as_sf()
 
-plotA <- johnston_sf %>%
-  ggplot(aes(fill = est_total_johnston)) +
+johnston_sf %>%
+  filter(indicator == "total") %>%
+  ggplot(aes(fill = estimate)) +
   geom_sf(size = 0.1, colour = scales::alpha("grey", 0.25)) +
   scale_fill_viridis_c(option = "C", na.value = "#E6E6E6") +
-  facet_grid(~age_group) +
-  labs(fill = "Total") +
-  theme_minimal()
+  facet_wrap(~age_group) +
+  labs(title = "Johnston total FSW estimate", fill = "Total") +
+  theme_void()
 
-plotB <- johnston_sf %>%
-  ggplot(aes(fill = est_proportion_johnston)) +
+johnston_sf %>%
+  filter(indicator == "proportion") %>%
+  ggplot(aes(fill = estimate)) +
   geom_sf(size = 0.1, colour = scales::alpha("grey", 0.25)) +
-  scale_fill_viridis_c(option = "C",  label = label_percent(), na.value = "#E6E6E6") +
-  facet_grid(~age_group) +
-  labs(fill = "Proportion") +
-  theme_minimal()
-
-cowplot::plot_grid(plotA, plotB, ncol = 1)
+  scale_fill_viridis_c(option = "C", na.value = "#E6E6E6") +
+  facet_wrap(~age_group) +
+  labs(title = "Johnston FSW proportion estimate", fill = "Proportion") +
+  theme_void()
 
 dev.off()
 
-#' The proportion estimates from the sexpaid12m category of our model (pre-adjustment)
+#' The proportion estimates from the sexpaid12m category of our model
+#' These are pre-adjustment and therefore correspond to transactional sex
 est <- read_csv("depends/best-3p1-multi-sexbehav-sae.csv") %>%
   multi.utils::update_naming() %>%
   filter(
     indicator == "FSW",
     age_group %in% c("15-19", "20-24", "25-29")
   ) %>%
-  #' Only the most recent survey in each year
-  group_by(iso3) %>%
-  filter(year == max(year)) %>%
+  filter(year == 2018) %>%
   select(iso3, indicator, survey_id, age_group, area_id, estimate_raw, estimate_smoothed)
 
-#' Calculate estimates of FSW populations by age and area using proportions from ind and PSE from naomi3
+#' Calculate estimates of FSW populations by age and area using
+#' * proportions from ind
+#' * population data from pop
 df <- est %>%
-  filter(!(area_id %in% priority_iso3)) %>%
   left_join(
-    naomi3 %>%
-      filter(iso3 %in% priority_iso3) %>%
-      select(age_group, area_id, population_mean),
+    pop %>%
+      select(age_group, area_id, population),
     by = c("age_group", "area_id")
   ) %>%
   mutate(
-    iso3 = substr(survey_id, 1, 3),
-    est_raw = estimate_raw * population_mean,
-    est_smoothed = estimate_smoothed * population_mean
+    iso3 = substr(area_id, 1, 3),
+    raw = estimate_raw * population,
+    smoothed = estimate_smoothed * population
   ) %>%
   group_by(iso3, age_group) %>%
   summarise(
-    est_total_raw = sum(est_raw, na.rm = TRUE),
-    est_total_smoothed = sum(est_smoothed, na.rm = TRUE),
-    population_mean = sum(population_mean, na.rm = TRUE)
+    total_raw = sum(raw, na.rm = TRUE),
+    total_smoothed = sum(smoothed, na.rm = TRUE),
+    population = sum(population, na.rm = TRUE)
   ) %>%
   mutate(
-    est_proportion_raw = est_total_raw / population_mean,
-    est_proportion_smoothed = est_total_smoothed / population_mean
+    proportion_raw = total_raw / population,
+    proportion_smoothed = total_smoothed / population
   )
 
 pdf("johnston-fsw-comparison.pdf", h = 11.25, w = 8.75)
 
 johnston_comparison <- johnston %>%
+  rename(total_johnston = total, proportion_johnston = proportion) %>%
   left_join(
-    select(df, iso3, age_group, est_proportion_raw, est_proportion_smoothed),
+    select(df, iso3, age_group, proportion_raw, proportion_smoothed),
     by = c("iso3", "age_group")
   ) %>%
   pivot_longer(
-    cols = starts_with("est_proportion"),
+    cols = starts_with("proportion"),
     names_to = "method",
-    names_prefix = "est_proportion_",
-    values_to = "est_proportion"
+    names_prefix = "proportion_",
+    values_to = "proportion"
   ) %>%
   mutate(method = fct_recode(method, "Raw" = "raw", "Johnston" = "johnston", "Smoothed" = "smoothed"))
-
-johnston_comparison %>%
-  ggplot(aes(x = method, y = est_proportion, fill = method)) +
-  geom_col() +
-  facet_wrap(iso3 ~ age_group, scales = "free") +
-  scale_fill_manual(values = multi.utils::cbpalette()) +
-  labs(x = "", y = "", fill = "") +
-  theme_minimal() +
-  theme(
-    legend.position = "bottom",
-    axis.title.x=element_blank(),
-    axis.text.x=element_blank(),
-    axis.ticks.x=element_blank()
-  )
 
 johnston_comparison %>%
   left_join(
@@ -217,17 +195,14 @@ johnston_comparison %>%
     by = "iso3"
   ) %>%
   st_as_sf() %>%
-  ggplot(aes(fill = est_proportion)) +
+  ggplot(aes(fill = proportion)) +
   geom_sf(size = 0.1, colour = scales::alpha("grey", 0.25)) +
   scale_fill_viridis_c(option = "C",  label = label_percent(), na.value = "#E6E6E6") +
   facet_grid(method ~ age_group) +
   labs(fill = "Proportion") +
-  theme_minimal() +
+  theme_void() +
   theme(
     legend.position = "bottom",
-    axis.title.x=element_blank(),
-    axis.text.x=element_blank(),
-    axis.ticks.x=element_blank()
   )
 
 dev.off()
@@ -235,11 +210,12 @@ dev.off()
 pdf("johnston-fsw-comparison-xy.pdf", h = 5, w = 6.25)
 
 johnston %>%
+  rename(total_johnston = total, proportion_johnston = proportion) %>%
   left_join(
-    select(df, iso3, age_group, est_proportion_raw, est_proportion_smoothed),
+    select(df, iso3, age_group, proportion_raw, proportion_smoothed),
     by = c("iso3", "age_group")
   ) %>%
-  ggplot(aes(x = est_proportion_johnston, y = est_proportion_smoothed, col = iso3)) +
+  ggplot(aes(x = proportion_johnston, y = proportion_smoothed, col = iso3)) +
   geom_point() +
   geom_abline(intercept = 0, slope = 1, linetype = "dashed") +
   facet_grid(age_group ~ .) +
@@ -261,33 +237,37 @@ laga <- laga %>%
   rename(
     iso3 = ISO,
     country = NAME_0,
-    est_total_laga = Fitted_FSW
+    total_laga = Fitted_FSW
   ) %>%
   select(-X, -ref_pop, -Uncertainty, -Upper, -Lower, -Prev, -Percent, -Predictor_only, -Predictor_only_rank) %>%
+  mutate(age_group = "15-49") %>%
+  filter(iso3 %in% priority_iso3) %>%
   left_join(
-    naomi3_national %>%
-      group_by(iso3) %>%
-      summarise(population_mean = sum(population_mean)),
-    by = "iso3"
+    pop %>%
+      filter(area_id %in% priority_iso3),
+    by = c("iso3", "age_group")
   )
 
 laga_comparison <- df %>%
   group_by(iso3) %>%
   summarise(
-    est_total_raw = sum(est_total_raw),
-    est_total_smoothed = sum(est_total_smoothed)
+    total_raw = sum(total_raw),
+    total_smoothed = sum(total_smoothed)
   ) %>%
-  left_join(select(laga, iso3, est_total_laga, population_mean), by = "iso3") %>%
+  left_join(
+    select(laga, iso3, total_laga, population),
+    by = "iso3"
+  ) %>%
   mutate(
-    est_proportion_raw = est_total_raw / population_mean,
-    est_proportion_smoothed = est_total_smoothed / population_mean,
-    est_proportion_laga = est_total_laga / population_mean
+    proportion_raw = total_raw / population,
+    proportion_smoothed = total_smoothed / population,
+    proportion_laga = total_laga / population
   ) %>%
   pivot_longer(
-    cols = starts_with("est_proportion"),
+    cols = starts_with("proportion"),
     names_to = "method",
-    names_prefix = "est_proportion_",
-    values_to = "est_proportion"
+    names_prefix = "proportion_",
+    values_to = "proportion"
   ) %>%
   mutate(method = fct_recode(method, "Raw" = "raw", "Laga" = "laga", "Smoothed" = "smoothed"))
 
@@ -296,37 +276,20 @@ write_csv(laga_comparison, "laga-fsw-comparison.csv", na = "")
 pdf("laga-fsw-comparison.pdf", h = 8, w = 8.75)
 
 laga_comparison %>%
-  ggplot(aes(x = method, y = est_proportion, fill = method)) +
-  geom_col() +
-  facet_wrap(iso3 ~ ., scales = "free") +
-  scale_fill_manual(values = multi.utils::cbpalette()) +
-  labs(x = "", y = "", fill = "") +
-  theme_minimal() +
-  theme(
-    legend.position = "bottom",
-    axis.title.x=element_blank(),
-    axis.text.x=element_blank(),
-    axis.ticks.x=element_blank()
-  )
-
-laga_comparison %>%
   left_join(
     select(national_areas, "iso3" = "area_id"),
     by = "iso3"
   ) %>%
   st_as_sf() %>%
-  ggplot(aes(fill = est_proportion)) +
+  ggplot(aes(fill = proportion)) +
   geom_sf(size = 0.1, colour = scales::alpha("grey", 0.25)) +
   scale_fill_viridis_c(option = "C",  label = label_percent(), na.value = "#E6E6E6") +
   facet_grid(~ method) +
   labs(fill = "Proportion") +
-  theme_minimal() +
+  theme_void() +
   theme(
     legend.position = "bottom",
-    legend.key.width = unit(4, "lines"),
-    axis.title.x=element_blank(),
-    axis.text.x=element_blank(),
-    axis.ticks.x=element_blank()
+    legend.key.width = unit(4, "lines")
   )
 
 dev.off()
@@ -336,20 +299,18 @@ pdf("laga-fsw-comparison-xy.pdf", h = 5, w = 6.25)
 df %>%
   group_by(iso3) %>%
   summarise(
-    est_total_raw = sum(est_total_raw),
-    est_total_smoothed = sum(est_total_smoothed)
+    total_smoothed = sum(total_smoothed)
   ) %>%
-  left_join(select(laga, iso3, est_total_laga, population_mean), by = "iso3") %>%
+  left_join(select(laga, iso3, total_laga, population), by = "iso3") %>%
   mutate(
-    est_proportion_raw = est_total_raw / population_mean,
-    est_proportion_smoothed = est_total_smoothed / population_mean,
-    est_proportion_laga = est_total_laga / population_mean
+    proportion_smoothed = total_smoothed / population,
+    proportion_laga = total_laga / population
   ) %>%
-  ggplot(aes(x = est_proportion_laga, y = est_proportion_smoothed, col = iso3)) +
+  ggplot(aes(x = proportion_laga, y = proportion_smoothed, col = iso3)) +
   geom_point() +
   geom_abline(intercept = 0, slope = 1, linetype = "dashed") +
   lims(x = c(0, 0.1), y = c(0, 0.1)) +
-  labs(x = "Laga estimate", y = "Smoothed estimate", col = "ISO3") +
+  labs(x = "Laga proportion estimate (15-49)", y = "Smoothed proportion estimate (15-29)", col = "ISO3") +
   theme_minimal() +
   theme(
     legend.position = "bottom"
@@ -374,13 +335,49 @@ oli_sf <- oli %>%
   ) %>%
   st_as_sf()
 
-plotA <- oli_sf %>%
+oli_sf %>%
   ggplot(aes(fill = median)) +
   geom_sf(size = 0.1, colour = scales::alpha("grey", 0.25)) +
   scale_fill_viridis_c(option = "C",  label = label_percent(), na.value = "#E6E6E6") +
   labs(fill = "Median proportion") +
   theme_minimal()
 
-plotA
+dev.off()
+
+#' Import age-disaggregated version of Oli's FSW data
+oli_age <- read_csv("depends/fsw-estimates.csv")
+
+oli_johnston_comparison <- johnston %>%
+  select(-country, -iso3, -sex, -population, - proportion) %>%
+  rename(total_johnston = total) %>%
+  left_join(
+    select(df, area_id = iso3, age_group, total_raw, total_smoothed),
+    by = c("area_id", "age_group")
+  ) %>%
+  left_join(
+    oli_age %>%
+      select(area_id, age_group, fsw) %>%
+      rename(total_oli = fsw) %>%
+      mutate(
+        age_group = recode_factor(
+          age_group,
+          "Y015_019" = "15-19", "Y020_024" = "20-24", "Y025_029" = "25-29"
+        )
+      ),
+    by = c("area_id", "age_group")
+  )
+
+pdf("oli-johnston-fsw-comparison-xy.pdf", h = 5, w = 6.25)
+
+oli_johnston_comparison %>%
+  filter(!is.na(total_johnston), !is.na(total_oli)) %>%
+  ggplot(aes(x = total_johnston, y = total_oli, col = area_id, shape = age_group)) +
+  geom_point(alpha = 0.8, size = 2) +
+  lims(x = c(0, 10^5), y = c(0, 10^5)) +
+  geom_abline(intercept = 0, slope = 1, linetype = "dashed", col = "grey") +
+  theme_minimal() +
+  labs(x = "Total FSW (Johnston)", y = "Total FSW (Oli)")
 
 dev.off()
+
+write_csv(oli_johnston_comparison, "oli-johnston-fsw-comparison.csv")
--- src/process_transactional-fsw-adjustment/script.R
+++ src/process_transactional-fsw-adjustment/script.R
@@ -2,65 +2,63 @@
 # orderly::orderly_develop_start("process_transactional-fsw-adjustment")
 # setwd("src/process_transactional-fsw-adjustment")
 
+#' Could add a new indicator name like ywkp or fsw rather than sexpaid12m
+#' This would help clarify that the estimate is now different
 df_3p1 <- read_csv("depends/best-3p1-multi-sexbehav-sae.csv")
-johnston <- read_csv("depends/johnston-fsw-comparison.csv")
+fsw <- read_csv("depends/oli-johnston-fsw-comparison.csv")
 
-johnston <- johnston %>%
-  select(iso3, age_group, method, est_proportion) %>%
-  filter(method %in% c("Smoothed", "Johnston")) %>%
-  pivot_wider(
-    values_from = est_proportion,
-    names_from = method
-  ) %>%
-  #' Such that estimate * ratio will scale the estimates to match Johnston at national level
-  mutate(johnston_fsw_ratio = Johnston / Smoothed) %>%
-  select(-Johnston, - Smoothed)
-
-#' Johnston doesn't include 25-29, so let's just assume it's the same ratio as in 20-24 for now
-johnston_Y025_029 <- johnston %>%
-  filter(age_group == "20-24") %>%
-  mutate(age_group = "25-29")
-
-johnston <- bind_rows(johnston, johnston_Y025_029) %>%
+fsw <- fsw %>%
+  mutate(transactional_fsw_ratio = total_oli / total_smoothed) %>%
+  select(age_group, iso3 = area_id, transactional_fsw_ratio) %>%
   mutate(age_group = fct_recode(age_group, "Y015_019" = "15-19", "Y020_024" = "20-24", "Y025_029" = "25-29"))
 
-perform_johnston_adjustment <- function(df, johnston) {
-  df_adjusted <- df_3p1 %>%
-    left_join(
-      johnston,
-      by = c("iso3", "age_group")
-    ) %>%
-    select(iso3, year, indicator, survey_id, age_group, area_id, area_name, estimate_smoothed, johnston_fsw_ratio) %>%
-    pivot_wider(
-      names_from = indicator,
-      values_from = estimate_smoothed
-    ) %>%
-    mutate(
-      sexpaid12m = ifelse(!is.na(johnston_fsw_ratio), sexpaid12m * johnston_fsw_ratio, sexpaid12m),
-      #' Rather than increasing by the amount that is lost from sexpaid12m, can just use the fact that must sum to 1
-      sexnonreg = 1 - nosex12m - sexcohab - sexpaid12m
-    ) %>%
-    select(-johnston_fsw_ratio) %>%
-    pivot_longer(
-      cols = nosex12m:sexpaid12m,
-      names_to = "indicator",
-      values_to = "estimate_smoothed"
-    )
-}
+df_3p1_adjusted <- df_3p1 %>%
+  left_join(
+    fsw,
+    by = c("iso3", "age_group")
+  ) %>%
+  select(iso3, year, indicator, survey_id, age_group, area_id, area_name, estimate_smoothed, transactional_fsw_ratio) %>%
+  pivot_wider(
+    names_from = indicator,
+    values_from = estimate_smoothed
+  ) %>%
+  mutate(
+    sexpaid12m = ifelse(
+      is.finite(transactional_fsw_ratio) & !is.na(transactional_fsw_ratio),
+      sexpaid12m * transactional_fsw_ratio, sexpaid12m
+    ),
+    #' Rather than increasing by the amount that is lost from sexpaid12m, can just use the fact that must sum to 1
+    sexnonreg = 1 - nosex12m - sexcohab - sexpaid12m
+  ) %>%
+  select(-transactional_fsw_ratio) %>%
+  pivot_longer(
+    cols = nosex12m:sexpaid12m,
+    names_to = "indicator",
+    values_to = "estimate_smoothed"
+  )
 
-df_3p1_adjusted <- perform_johnston_adjustment(df_3p1, johnston)
 write_csv(df_3p1_adjusted, "adjust-best-3p1-multi-sexbehav-sae.csv")
 
-pdf("quick-adjustment-comparison.pdf", h = 5, w = 6.25)
+pdf("adjustment-comparison.pdf", h = 5, w = 6.25)
 
-df_3p1 %>%
-  filter(indicator == "sexpaid12m") %>%
-  pull(estimate_smoothed) %>%
-  plot(main = "No adjustment")
-
-df_3p1_adjusted %>%
-  filter(indicator == "sexpaid12m") %>%
-  pull(estimate_smoothed) %>%
-  plot(main = "Johnston adjustment")
+bind_rows(
+  df_3p1 %>%
+    filter(indicator == "sexpaid12m", year == 2018) %>%
+    mutate(adjustment = "Unadjusted") %>%
+    select(age_group, area_id, estimate_smoothed, adjustment),
+  df_3p1_adjusted %>%
+    filter(indicator == "sexpaid12m", year == 2018) %>%
+    mutate(adjustment = "Adjusted") %>%
+    select(age_group, area_id, estimate_smoothed, adjustment)
+) %>%
+  pivot_wider(
+    names_from = adjustment,
+    values_from = estimate_smoothed
+  ) %>%
+  mutate(iso3 = substr(area_id, 1, 3)) %>%
+  ggplot(aes(x = Unadjusted, y = Adjusted, col = iso3, shape = age_group)) +
+  geom_point(alpha = 0.5) +
+  lims(x = c(0, 0.1), y = c(0, 0.1)) +
+  theme_minimal()
 
 dev.off()
