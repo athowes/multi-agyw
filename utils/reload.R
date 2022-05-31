94316dacb9802ab249bb45329dc80e5d3851f8ae
athowes <adamhowes@outlook.com>
2022-05-31 16:25:54 +0100

    Might need to reload multi.utils too

--- utils/reload.R
+++ utils/reload.R
@@ -1,3 +1,6 @@
 #' Reload naomi.utils (saves restarting R)
 detach("package:naomi.utils", unload = TRUE)
 library(naomi.utils)
+
+# detach("package:multi.utils", unload = TRUE)
+# library(multi.utils)
