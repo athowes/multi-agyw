5fdc10db67ddb93934c153a543a5020b18cccdc2
athowes <adamhowes@outlook.com>
2022-06-15 09:29:55 -0400

    Ran docs_paper to send to Imperial coauthors, added Katie's feedback on abstract #128

--- make/8_docs.R
+++ make/8_docs.R
@@ -1,2 +1,10 @@
 run_commit_push("plot-tikz_category-flowchart")
+
+#' Presentations
+run_commit_push("docs_12-05-22-mrc-gida")
+
+#' Posters
 run_commit_push("docs_amazon-poster")
+
+#' Paper and appendices
+run_commit_push("docs_paper")
