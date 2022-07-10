80657ff96920c525d02ab4b9f0c74f390a56b901
athowes <adamhowes@outlook.com>
2022-07-10 09:06:53 -0400

    Add orderly bundles workflow #138

--- make/5_run_fit_multi-sexbehav-sae.R
+++ make/5_run_fit_multi-sexbehav-sae.R
@@ -8,10 +8,73 @@ id <- orderly::orderly_run("fit_multi-sexbehav-sae", parameters = list(lightweig
 orderly::orderly_commit(id) #' [x]
 
 id <- orderly::orderly_run("fit_multi-sexbehav-sae", parameters = list(fewer_countries = TRUE))
-orderly::orderly_commit(id) #' [ ]
+orderly::orderly_commit(id) #' [x]
 
 id <- orderly::orderly_run("fit_multi-sexbehav-sae", parameters = list(lightweight = TRUE))
 orderly::orderly_commit(id) #' [x]
 
 id <- orderly::orderly_run("fit_multi-sexbehav-sae", parameters = list(include_interactions = FALSE))
 orderly::orderly_commit(id) #' [x]
+
+#' The full version with interactions is too slow to run locally,
+#' so let's try using the orderly bundles workflow
+
+#' A. On local machine:
+#' 1. Create bundle
+#' 2. Upload to sharepoint somewhere with spud
+#' B. On windows VM:
+#' 1. Pull bundle
+#' 2. Run bundle
+#' 3. Upload to sharepoint
+#' C. On local machine
+#' 1. Pull bundle
+#' 2. Import into archive
+
+#' A1.
+path_bundles <- "bundle-input"
+bundle <- orderly::orderly_bundle_pack(
+  path = path_bundles,
+  name = "fit_multi-sexbehav-sae",
+  parameters = list(include_interactions = TRUE)
+)
+
+#' A2.
+spud <- spud::sharepoint$new("https://imperiallondon.sharepoint.com/")
+folder <- spud$folder("HIVInferenceGroup-WP", "Shared Documents/orderly/multi-agyw/bundle-input", verify = TRUE)
+folder$upload(path = bundle$path)
+
+recent_bundle <- folder$list() %>%
+  filter(created == max(created))
+
+#' B1.
+folder$download(
+  path = recent_bundle$name,
+  dest = paste0("bundle-input/", recent_bundle$name)
+)
+
+#' B2.
+orderly_packages <- yaml::read_yaml(file.path("src/fit_multi-sexbehav-sae/orderly.yml"))$packages
+packages <- list(loaded = c("orderly", orderly_packages))
+
+config <- didehpc::didehpc_config(
+  workdir = path_bundles,
+  credentials = "ath19",
+  cluster = "fi--didemrchnb"
+  # "fi--dideclusthn"
+  # "fi--didemrchnb"
+)
+
+src <- conan::conan_sources("athowes/multi.utils")
+
+ctx <- context::context_save(
+  "context",
+  packages = packages,
+  package_sources = src
+)
+
+obj <- didehpc::queue_didehpc(ctx, config = config)
+
+t <- obj$enqueue(orderly::orderly_bundle_run(recent_bundle$name, "bundle-output"))
+
+t$status()
+t$result()
