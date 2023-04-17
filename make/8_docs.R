80b0ce73f1b050bfb50f827cd2e0829e5ffd97c8
athowes <adamthowes@gmail.com>
2023-04-17 19:09:43 +0100

    Start sketching out slides for #144

--- make/8_docs.R
+++ make/8_docs.R
@@ -11,6 +11,7 @@ run_commit_push("docs_12-05-22-mrc-gida")
 run_commit_push("docs_28-09-22-toronto")
 run_commit_push("docs_06-10-22-hdsl")
 run_commit_push("docs_14-10-22-waterloo")
+run_commit_push("docs_17-05-23-hi-med")
 
 #' Posters
 run_commit_push("docs_amazon-poster")
--- src/docs_xx-05-23-hi-med/script.R
+++
@@ -1,4 +0,0 @@
-# orderly::orderly_develop_start("docs_xx-05-23-hi-med")
-# setwd("src/docs_xx-05-23-hi-med")
-
-rmarkdown::render("xx-05-23-hi-med.Rmd")
