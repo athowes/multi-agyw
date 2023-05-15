bfb81a9772e0f39b66e4485f0a8a67b6a0027a43
athowes <adamthowes@gmail.com>
2023-05-15 16:44:48 +0100

    Full outline draft of HiMed slides #144

--- src/docs_17-05-23-hi-med/script.R
+++ src/docs_17-05-23-hi-med/script.R
@@ -1,4 +1,4 @@
-# orderly::orderly_develop_start("docs_xx-05-23-hi-med")
+# orderly::orderly_develop_start("docs_17-05-23-hi-med")
 # setwd("src/docs_17-05-23-hi-med")
 
 rmarkdown::render("17-05-23-hi-med.Rmd")
